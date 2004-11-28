1b6f6c192142d6815de2db0302b8335cd55d70fe
Ozkan Sezer <sezeroz@gmail.com>
2004-11-28 01:51:12 +0000

    sync with steven
    git-svn-id: file:///svn/uhexen2/code/trunk@36 eaf649e6-6c13-0410-b194-84221022858e

--- hexen2/build_all.sh
+++ hexen2/build_all.sh
@@ -3,18 +3,10 @@
 MAKEFILE=Makefile.packaging
 
 make -f $MAKEFILE clean
-make -f $MAKEFILE glmp_static
-#make -f $MAKEFILE clean
 make -f $MAKEFILE glmp_dynamic
 make -f $MAKEFILE clean
-make -f $MAKEFILE glh2_static
-#make -f $MAKEFILE clean
 make -f $MAKEFILE glh2_dynamic 
 make -f $MAKEFILE clean
-make -f $MAKEFILE mp_static
-#make -f $MAKEFILE clean 
 make -f $MAKEFILE mp_dynamic
 make -f $MAKEFILE clean
-make -f $MAKEFILE h2_static
-#make -f $MAKEFILE clean
 make -f $MAKEFILE h2_dynamic
