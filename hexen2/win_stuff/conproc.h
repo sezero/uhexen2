df373d81e25d2c3d3c0d7d38cb227d676edadddb
Ozkan Sezer <sezeroz@gmail.com>
2005-05-22 11:55:25 +0000

    conproc.* is for win32 only
    git-svn-id: file:///svn/uhexen2/code/trunk@596 eaf649e6-6c13-0410-b194-84221022858e

---
+++ hexen2/win_stuff/conproc.h
@@ -0,0 +1,18 @@
+// conproc.h
+
+#define CCOM_WRITE_TEXT		0x2
+// Param1 : Text
+
+#define CCOM_GET_TEXT		0x3
+// Param1 : Begin line
+// Param2 : End line
+
+#define CCOM_GET_SCR_LINES	0x4
+// No params
+
+#define CCOM_SET_SCR_LINES	0x5
+// Param1 : Number of lines
+
+void InitConProc (HANDLE hFile, HANDLE heventParent, HANDLE heventChild);
+void DeinitConProc (void);
+
