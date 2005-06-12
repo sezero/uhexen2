18780830a79c8422e7938cd51a1bb8d7aff4fce8
Ozkan Sezer <sezeroz@gmail.com>
2005-06-12 07:34:08 +0000

    updated all makefiles and build scripts. now I can happily do make CC=xxx ..., make/gmake issue should also be fixed.
    git-svn-id: file:///svn/uhexen2/code/trunk@701 eaf649e6-6c13-0410-b194-84221022858e

--- hexen2/build_all.sh
+++ hexen2/build_all.sh
@@ -2,12 +2,25 @@
 
 MAKEFILE=Makefile.packaging
 
-make -f $MAKEFILE clean
-make -f $MAKEFILE h2_dynamic
-make -f $MAKEFILE clean
-make -f $MAKEFILE glh2_dynamic 
-make -f $MAKEFILE clean
-make -f $MAKEFILE mp_dynamic
-make -f $MAKEFILE clean
-make -f $MAKEFILE glmp_dynamic
-make -f $MAKEFILE clean
+if [ -f /usr/local/bin/make ]; then
+	MAKE=/usr/local/bin/make
+elif [ -f /usr/bin/make ]; then
+	MAKE=/usr/bin/make
+elif [ -f /usr/local/bin/gmake ]; then
+	MAKE=/usr/local/bin/gmake
+elif [ -f /usr/bin/gmake ]; then
+	MAKE=/usr/bin/gmake
+else
+	echo "Error: make or gmake not found"
+	exit 1
+fi
+
+$MAKE -f $MAKEFILE clean
+$MAKE -f $MAKEFILE h2_dynamic
+$MAKE -f $MAKEFILE clean
+$MAKE -f $MAKEFILE glh2_dynamic 
+$MAKE -f $MAKEFILE clean
+$MAKE -f $MAKEFILE mp_dynamic
+$MAKE -f $MAKEFILE clean
+$MAKE -f $MAKEFILE glmp_dynamic
+$MAKE -f $MAKEFILE clean
--- hexenworld/Client/build_all.sh
+++ hexenworld/Client/build_all.sh
@@ -2,8 +2,21 @@
 
 MAKEFILE=Makefile.packaging
 
-make -f $MAKEFILE clean
-make -f $MAKEFILE hw_dynamic 
-make -f $MAKEFILE clean
-make -f $MAKEFILE glhw_dynamic
-make -f $MAKEFILE clean
+if [ -f /usr/local/bin/make ]; then
+	MAKE=/usr/local/bin/make
+elif [ -f /usr/bin/make ]; then
+	MAKE=/usr/bin/make
+elif [ -f /usr/local/bin/gmake ]; then
+	MAKE=/usr/local/bin/gmake
+elif [ -f /usr/bin/gmake ]; then
+	MAKE=/usr/bin/gmake
+else
+	echo "Error: make or gmake not found"
+	exit 1
+fi
+
+$MAKE -f $MAKEFILE clean
+$MAKE -f $MAKEFILE hw_dynamic 
+$MAKE -f $MAKEFILE clean
+$MAKE -f $MAKEFILE glhw_dynamic
+$MAKE -f $MAKEFILE clean
