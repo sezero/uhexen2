5e69c16f5115e176f021be9d30cf064d797507e1
Ozkan Sezer <sezeroz@gmail.com>
2005-09-19 19:29:42 +0000

    prevent (in fact, partially) coop and deathmatch cvars being both set at the same time (from Maddes / QIP)
    git-svn-id: file:///svn/uhexen2/code/trunk@1059 eaf649e6-6c13-0410-b194-84221022858e

--- hexen2/net_main.c
+++ hexen2/net_main.c
@@ -190,9 +190,17 @@ static void MaxPlayers_f (void)
 
 	svs.maxclients = n;
 	if (n == 1)
+	{
 		Cvar_Set ("deathmatch", "0");
+		Cvar_Set ("coop", "0");
+	}
 	else
-		Cvar_Set ("deathmatch", "1");
+	{
+		if (coop.value)
+			Cvar_Set ("deathmatch", "0");
+		else
+			Cvar_Set ("deathmatch", "1");
+	}
 }
 
 
