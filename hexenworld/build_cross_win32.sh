7111c27e6b96516972efe799658938488161485f
Ozkan Sezer <sezeroz@gmail.com>
2005-09-21 21:13:45 +0000

    fixed oversight in win32 cross compile scripts
    git-svn-id: file:///svn/uhexen2/code/trunk@1073 eaf649e6-6c13-0410-b194-84221022858e

--- hexenworld/build_cross_win32.sh
+++ hexenworld/build_cross_win32.sh
@@ -13,17 +13,17 @@ export PATH
 MAKEFILE=Makefile.mingw
 
 echo "Building HexenWorld Server"
-make -C Server -f $MAKEFILE WIN32CC=$TARGET-gcc MINGWDIR=$TARGET $*
+make -C Server -f $MAKEFILE WIN32CC=$TARGET-gcc MINGWDIR=$PREFIX/$TARGET $*
 
 echo "" && echo "Building HexenWorld Master Server"
-make -C Master -f $MAKEFILE WIN32CC=$TARGET-gcc MINGWDIR=$TARGET $*
+make -C Master -f $MAKEFILE WIN32CC=$TARGET-gcc MINGWDIR=$PREFIX/$TARGET $*
 
 echo "" && echo "Building HexenWorld Client (Software renderer)"
-make -C Client -f $MAKEFILE WIN32CC=$TARGET-gcc WIN32AS=nasm WIN32RES=$TARGET-windres MINGWDIR=$TARGET $* hw_exe
+make -C Client -f $MAKEFILE WIN32CC=$TARGET-gcc WIN32AS=nasm WIN32RES=$TARGET-windres MINGWDIR=$PREFIX/$TARGET $* hw_exe
 
 echo "" && echo "Building HexenWorld Client (OpenGL renderer)"
 make -C Client -f $MAKEFILE clean
-make -C Client -f $MAKEFILE WIN32CC=$TARGET-gcc WIN32AS=nasm WIN32RES=$TARGET-windres MINGWDIR=$TARGET $* glhw_exe
+make -C Client -f $MAKEFILE WIN32CC=$TARGET-gcc WIN32AS=nasm WIN32RES=$TARGET-windres MINGWDIR=$PREFIX/$TARGET $* glhw_exe
 
 echo "" && echo "Stripping all HexenWorld binaries"
 make -C Server -f $MAKEFILE W32STRIP=$TARGET-strip strip
