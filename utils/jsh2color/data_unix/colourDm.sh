d7f617b4fa8e826888a263ad286d4edffab9a02e
Ozkan Sezer <sezeroz@gmail.com>
2006-02-12 11:11:11 +0000

    added unix-style versions of data files
    git-svn-id: file:///svn/uhexen2/code/trunk@1315 eaf649e6-6c13-0410-b194-84221022858e

---
+++ utils/jsh2color/data_unix/colourDm.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+jsh2colour -lit -extra -force -light 20  ravdm1
+jsh2colour -lit -extra -force -light 20  ravdm2
+jsh2colour -lit -extra -force -light 20  ravdm3
+jsh2colour -lit -extra -force -light 20  ravdm4
+jsh2colour -lit -extra -force -light 20  ravdm5
---
+++ utils/jsh2color/data_unix/colourMeso.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+jsh2colour -lit -extra -force -light 30 -external hexen2meso.def -nodefault meso1
+jsh2colour -lit -extra -force -light 30 -external hexen2meso.def -nodefault meso2
+jsh2colour -lit -extra -force -light 30 -external hexen2meso.def -nodefault meso3
+jsh2colour -lit -extra -force -light 30 -external hexen2meso.def -nodefault meso4
+jsh2colour -lit -extra -force -light 30 -external hexen2meso.def -nodefault meso5
+jsh2colour -lit -extra -force -light 30 -external hexen2meso.def -nodefault meso6
+jsh2colour -lit -extra -force -light 30 -external hexen2meso.def -nodefault meso8
+jsh2colour -lit -extra -force -light 30 -external hexen2meso.def -nodefault meso9
