ae9716e99637416ddd603c84a1ebe1ebfe110102
Ozkan Sezer <sezeroz@gmail.com>
2006-02-12 12:22:03 +0000

    bumped version to 1.4.0-pre3 (2006-02-12)
    git-svn-id: file:///svn/uhexen2/code/trunk@1327 eaf649e6-6c13-0410-b194-84221022858e

--- hexen2/quakedef.h
+++ hexen2/quakedef.h
@@ -2,7 +2,7 @@
 	quakedef.h
 	primary header for client
 
-	$Header: /home/ozzie/Download/0000/uhexen2/hexen2/quakedef.h,v 1.43 2006-01-24 17:09:27 sezero Exp $
+	$Header: /home/ozzie/Download/0000/uhexen2/hexen2/quakedef.h,v 1.44 2006-02-12 12:22:02 sezero Exp $
 */
 
 //#define	GLTEST			// experimental stuff
@@ -25,8 +25,8 @@
 #define	HOT_VERSION_MIN		0
 #define	HOT_VERSION_BETA	1
 #if HOT_VERSION_BETA
-#define	HOT_VERSION_BETA_STR	"pre2"
-#define	HOT_VERSION_BETA_DATE	"2006-01-24"
+#define	HOT_VERSION_BETA_STR	"pre3"
+#define	HOT_VERSION_BETA_DATE	"2006-02-12"
 #endif
 #define	HOT_VERSION_STR		STRINGIFY(HOT_VERSION_MAJ) "." STRINGIFY(HOT_VERSION_MID) "." STRINGIFY(HOT_VERSION_MIN)
 #define	GLQUAKE_VERSION		1.00
@@ -400,6 +400,9 @@ void MIDI_UpdateVolume(void);
 
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.43  2006/01/24 17:09:27  sezero
+ * bumped version to 1.4.0-pre2 (2006-01-24)
+ *
  * Revision 1.42  2006/01/23 20:22:53  sezero
  * tidied up the version and help display stuff. bumped the HoT version to
  * 1.4.0-pre1. added conditionals to properly display beta version strings.
