6bd757cf81e9c0396381738c5b2caca52921ced4
Ozkan Sezer <sezeroz@gmail.com>
2006-02-22 17:58:51 +0000

    continue making static functions and vars static. whitespace and coding style cleanup. (part 22: wad.c, wad.h).
    git-svn-id: file:///svn/uhexen2/code/trunk@1358 eaf649e6-6c13-0410-b194-84221022858e

--- hexen2/wad.c
+++ hexen2/wad.c
@@ -5,7 +5,7 @@
 int			wad_numlumps;
 lumpinfo_t	*wad_lumps;
 byte		*wad_base = NULL;
-int		wad_len;
+static int		wad_len;
 
 void SwapPic (qpic_t *pic);
 
@@ -20,26 +20,25 @@ Space padding is so names can be printed nicely in tables.
 Can safely be performed in place.
 ==================
 */
-void W_CleanupName (char *in, char *out)
+static void W_CleanupName (char *in, char *out)
 {
 	int		i;
 	int		c;
-	
+
 	for (i=0 ; i<16 ; i++ )
 	{
 		c = in[i];
 		if (!c)
 			break;
-			
+
 		if (c >= 'A' && c <= 'Z')
 			c += ('a' - 'A');
 		out[i] = c;
 	}
-	
+
 	for ( ; i< 16 ; i++ )
 		out[i] = 0;
 }
-
 
 
 /*
@@ -51,16 +50,16 @@ void W_LoadWadFile (char *filename)
 {
 	lumpinfo_t		*lump_p;
 	wadinfo_t		*header;
-	unsigned		i;
-	int				infotableofs;
-	
+	int			i;
+	int			infotableofs;
+
 	// Pa3PyX: check if the file has already been loaded. If so, reload at
 	//	   the same location.
 	// FIXME: Loading another wad (with bigger size) on top of a smaller one
-	//        may be treacherous (the hunk is purged upon changing maps),
-	//        although the game never does that except upon changing video
-	//        modes in OpenGL, where the wad file is the same (so it should
-	//        load on top of the old one with no problems).
+	//	  may be treacherous (the hunk is purged upon changing maps),
+	//	  although the game never does that except upon changing video
+	//	  modes in OpenGL, where the wad file is the same (so it should
+	//	  load on top of the old one with no problems).
 	if (wad_base)
 	{
 		wad_base = COM_LoadBufFile(filename, wad_base, &wad_len);
@@ -74,17 +73,17 @@ void W_LoadWadFile (char *filename)
 		Sys_Error ("W_LoadWadFile: couldn't load %s", filename);
 
 	header = (wadinfo_t *)wad_base;
-	
+
 	if (header->identification[0] != 'W'
-	|| header->identification[1] != 'A'
-	|| header->identification[2] != 'D'
-	|| header->identification[3] != '2')
+			|| header->identification[1] != 'A'
+			|| header->identification[2] != 'D'
+			|| header->identification[3] != '2')
 		Sys_Error ("Wad file %s doesn't have WAD2 id\n",filename);
-		
+
 	wad_numlumps = LittleLong(header->numlumps);
 	infotableofs = LittleLong(header->infotableofs);
 	wad_lumps = (lumpinfo_t *)(wad_base + infotableofs);
-	
+
 	for (i=0, lump_p = wad_lumps ; i<wad_numlumps ; i++,lump_p++)
 	{
 		lump_p->filepos = LittleLong(lump_p->filepos);
@@ -101,20 +100,20 @@ void W_LoadWadFile (char *filename)
 W_GetLumpinfo
 =============
 */
-lumpinfo_t	*W_GetLumpinfo (char *name)
+static lumpinfo_t *W_GetLumpinfo (char *name)
 {
 	int		i;
 	lumpinfo_t	*lump_p;
 	char	clean[16];
-	
+
 	W_CleanupName (name, clean);
-	
+
 	for (lump_p=wad_lumps, i=0 ; i<wad_numlumps ; i++,lump_p++)
 	{
 		if (!strcmp(clean, lump_p->name))
 			return lump_p;
 	}
-	
+
 	Sys_Error ("W_GetLumpinfo: %s not found", name);
 	return NULL;
 }
@@ -122,23 +121,25 @@ lumpinfo_t	*W_GetLumpinfo (char *name)
 void *W_GetLumpName (char *name)
 {
 	lumpinfo_t	*lump;
-	
+
 	lump = W_GetLumpinfo (name);
-	
+
 	return (void *)(wad_base + lump->filepos);
 }
 
+#if 0	// no users
 void *W_GetLumpNum (int num)
 {
 	lumpinfo_t	*lump;
-	
+
 	if (num < 0 || num > wad_numlumps)
 		Sys_Error ("W_GetLumpNum: bad number: %i", num);
-		
+
 	lump = wad_lumps + num;
-	
+
 	return (void *)(wad_base + lump->filepos);
 }
+#endif
 
 /*
 =============================================================================
@@ -151,5 +152,5 @@ automatic byte swapping
 void SwapPic (qpic_t *pic)
 {
 	pic->width = LittleLong(pic->width);
-	pic->height = LittleLong(pic->height);	
+	pic->height = LittleLong(pic->height);
 }
--- hexenworld/Client/wad.h
+++ hexenworld/Client/wad.h
@@ -10,7 +10,7 @@
 #define	TYP_NONE		0
 #define	TYP_LABEL		1
 
-#define	TYP_LUMPY		64				// 64 + grab command number
+#define	TYP_LUMPY		64	// 64 + grab command number
 #define	TYP_PALETTE		64
 #define	TYP_QTEX		65
 #define	TYP_QPIC		66
@@ -20,14 +20,12 @@
 typedef struct
 {
 	int			width, height;
-	byte		data[4];			// variably sized
+	byte		data[4];		// variably sized
 } qpic_t;
-
-
 
 typedef struct
 {
-	char		identification[4];		// should be WAD2 or 2DAW
+	char		identification[4];	// should be WAD2 or 2DAW
 	int			numlumps;
 	int			infotableofs;
 } wadinfo_t;
@@ -36,11 +34,11 @@ typedef struct
 {
 	int			filepos;
 	int			disksize;
-	int			size;					// uncompressed
+	int			size;		// uncompressed
 	char		type;
 	char		compression;
 	char		pad1, pad2;
-	char		name[16];				// must be null terminated
+	char		name[16];		// must be null terminated
 } lumpinfo_t;
 
 extern	int			wad_numlumps;
@@ -48,9 +46,8 @@ extern	lumpinfo_t	*wad_lumps;
 extern	byte		*wad_base;
 
 void	W_LoadWadFile (char *filename);
-void	W_CleanupName (char *in, char *out);
-lumpinfo_t	*W_GetLumpinfo (char *name);
 void	*W_GetLumpName (char *name);
 void	*W_GetLumpNum (int num);
 
-void SwapPic (qpic_t *pic);
+void	SwapPic (qpic_t *pic);
+
