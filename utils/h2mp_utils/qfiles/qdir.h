f27e218e713e29fbb8a1ad9356237485af9227b6
Ozkan Sezer <sezeroz@gmail.com>
2006-02-28 15:34:36 +0000

    fixed compilation of genmodel
    git-svn-id: file:///svn/uhexen2/code/trunk@1390 eaf649e6-6c13-0410-b194-84221022858e

--- utils/h2mp_utils/qfiles/qdir.h
+++ utils/h2mp_utils/qfiles/qdir.h
@@ -1,6 +1,8 @@
 
 extern	char		qdir[1024];
 extern	char		gamedir[1024];
+extern	qboolean	archive;
+extern	char		archivedir[1024];
 
 void	SetQdirFromPath (char *path);
 char	*ExpandArg (char *path);	// from cmd line
