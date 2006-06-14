0f54aa088f8e613e98ea90be63c0a9a745dc349f
Ozkan Sezer <sezeroz@gmail.com>
2006-06-14 12:54:44 +0000

    Updated CHANGES: version 1.4.1-pre4 / 2006-06-14
    git-svn-id: file:///svn/uhexen2/code/trunk@1670 eaf649e6-6c13-0410-b194-84221022858e

--- hexen2/quakedef.h
+++ hexen2/quakedef.h
@@ -2,7 +2,7 @@
 	quakedef.h
 	primary header for client
 
-	$Header: /home/ozzie/Download/0000/uhexen2/hexen2/quakedef.h,v 1.76 2006-06-10 13:56:38 sezero Exp $
+	$Header: /home/ozzie/Download/0000/uhexen2/hexen2/quakedef.h,v 1.77 2006-06-14 12:54:44 sezero Exp $
 */
 
 
@@ -18,9 +18,9 @@
 #define	HOT_VERSION_MAJ		1
 #define	HOT_VERSION_MID		4
 #define	HOT_VERSION_MIN		1
-#define	HOT_VERSION_REL_DATE	"2006-06-10"
+#define	HOT_VERSION_REL_DATE	"2006-06-14"
 #define	HOT_VERSION_BETA	1
-#define	HOT_VERSION_BETA_STR	"pre3"
+#define	HOT_VERSION_BETA_STR	"pre4"
 #define	HOT_VERSION_STR		STRINGIFY(HOT_VERSION_MAJ) "." STRINGIFY(HOT_VERSION_MID) "." STRINGIFY(HOT_VERSION_MIN)
 #define	GLQUAKE_VERSION		1.00
 #define	ENGINE_VERSION		1.15
@@ -437,6 +437,9 @@ void Chase_Update (void);
 
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.76  2006/06/10 13:56:38  sezero
+ * 1.4.1-pre3 / 2006-06-10
+ *
  * Revision 1.75  2006/06/08 20:28:55  sezero
  * 1.4.1-pre3 / 2006-06-08
  *
