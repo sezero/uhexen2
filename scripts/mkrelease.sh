a2f5e8fa68cfb89fca5548a52da45d89ddcad151
Ozkan Sezer <sezeroz@gmail.com>
2006-07-15 22:11:55 +0000

    updated the mkrelease script to run on non-linux platforms
    git-svn-id: file:///svn/uhexen2/code/trunk@1744 eaf649e6-6c13-0410-b194-84221022858e

--- scripts/mkrelease.sh
+++ scripts/mkrelease.sh
@@ -6,7 +6,7 @@ if echo `pwd` | grep "\/scripts" &>/dev/null ; then
 cd ..
 fi
 # this relies on the name of this script file being "mkrelease.sh"
-if [[ ! -f scripts/mkrelease.sh ]] ; then
+if [ ! -f scripts/mkrelease.sh ] ; then
 echo "change into the cvs snapshot directory before running this script"
 exit 1
 fi
@@ -16,7 +16,7 @@ GAMECODE_VER=1.15
 
 # kill the unwanted CVS* directories
 rm -rf CVSROOT
-find -name CVS | xargs rm -rf 
+find . -name CVS | xargs rm -rf 
 
 # kill the obsoleted empty directories
 rm -rf utils/h2_utils utils/h2mp_utils utils/dcc/bin
@@ -28,7 +28,7 @@ mv packaging/hexen2.spec ./
 rm -rf packaging
 
 # change all shell scripts' permissions
-find -name *.sh | xargs chmod 755 
+find . -name *.sh | xargs chmod 755 
 
 # move the unused/reference asm files directory into 00_unused
 mv asm 00_unused/asm_ref
