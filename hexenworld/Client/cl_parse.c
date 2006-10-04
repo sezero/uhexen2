adaf63cbd71889ca2cd795ecfe61b28989199e23
Ozkan Sezer <sezeroz@gmail.com>
2006-10-04 15:19:17 +0000

    removed an old comment in CL_ParseServerData.
    git-svn-id: file:///svn/uhexen2/code/trunk@1966 eaf649e6-6c13-0410-b194-84221022858e

--- hexenworld/Client/cl_parse.c
+++ hexenworld/Client/cl_parse.c
@@ -437,11 +437,6 @@ static void CL_ParseServerData (void)
 
 	Con_DPrintf ("Serverdata packet received.\n");
 
-/* if this is GL version, we need to tell D_FlushCaches() whether to flush
-   OGL textures depending on mapname change, but we cannot detect the map
-   name change here. So it will only check gl_purge_maptex.value. Servers
-   normally wouldn't repeat a map anyway, so no worries.
-*/
 //
 // wipe the client_state_t struct
 //
