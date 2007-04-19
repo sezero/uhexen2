1466626bd54edb64845035435fedc1212f6227ee
Ozkan Sezer <sezeroz@gmail.com>
2007-04-19 09:08:06 +0000

    updated the compiler.h header.
    git-svn-id: file:///svn/uhexen2/code/trunk@2484 eaf649e6-6c13-0410-b194-84221022858e

--- hw_utils/hwrcon/compiler.h
+++ hw_utils/hwrcon/compiler.h
@@ -3,18 +3,17 @@
 	compiler specific definitions and settings
 	used in the uhexen2 (Hammer of Thyrion) tree.
 
-	$Id: compiler.h,v 1.2 2007-03-14 21:04:22 sezero Exp $
+	$Id: compiler.h,v 1.3 2007-04-19 09:07:59 sezero Exp $
 */
 
 #ifndef __HX2_COMPILER_H
 #define __HX2_COMPILER_H
 
-#ifdef __GNUC__
-#define _FUNC_PRINTF(n) __attribute__((format (printf, n, n+1)))
-#else
-#define _FUNC_PRINTF(n)
+#if !defined(__GNUC__)
+#define	__attribute__(x)
 #endif	/* __GNUC__ */
 
+#define _FUNC_PRINTF(n) __attribute__((format (printf, n, n+1)))
 
 #endif	/* __HX2_COMPILER_H */
 
