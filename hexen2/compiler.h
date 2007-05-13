3de9bf151cf6b52a7f6e36aae9cecffccd775aa5
Ozkan Sezer <sezeroz@gmail.com>
2007-05-13 11:48:36 +0000

    added __thisfunc__ as a new macro and added preprocessor stuff for its definition in order to workaround different __FUNCTION__ and __func__ availability and behavior. __func__ of C99 is the default.
    git-svn-id: file:///svn/uhexen2/code/trunk@2548 eaf649e6-6c13-0410-b194-84221022858e

--- hexen2/compiler.h
+++ hexen2/compiler.h
@@ -3,7 +3,7 @@
 	compiler specific definitions and settings
 	used in the uhexen2 (Hammer of Thyrion) tree.
 
-	$Id: compiler.h,v 1.4 2007-04-19 13:59:39 sezero Exp $
+	$Id: compiler.h,v 1.5 2007-05-13 11:48:33 sezero Exp $
 */
 
 #ifndef __HX2_COMPILER_H
@@ -12,6 +12,24 @@
 #if !defined(__GNUC__)
 #define	__attribute__(x)
 #endif	/* __GNUC__ */
+
+
+#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L
+#define	__thisfunc__	__func__
+#elif defined(__GNUC__) && __GNUC__ < 3
+#define	__thisfunc__	__FUNCTION__
+#elif defined(__GNUC__) && __GNUC__ > 2
+#define	__thisfunc__	__func__
+#elif defined(__WATCOMC__)
+#define	__thisfunc__	__FUNCTION__
+#elif defined(__LCC__)
+#define	__thisfunc__	__func__
+#elif defined(_MSC_VER) && _MSC_VER >= 1300	/* VC7++ */
+#define	__thisfunc__	__FUNCTION__
+#else	/* stupid fallback */
+#error	__func__ or __FUNCTION__ compiler token not supported? define one...
+//#define	__thisfunc__	__FILE__
+#endif
 
 
 #endif	/* __HX2_COMPILER_H */
