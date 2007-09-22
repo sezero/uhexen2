5bc45253d75acc3ee09559d7fa0a0dce570d05ca
Ozkan Sezer <sezeroz@gmail.com>
2007-09-22 16:11:06 +0000

    undefine HAVE_STRLCPY and HAVE_STRLCAT in strl_fn.h (just in case..)
    git-svn-id: file:///svn/uhexen2/code/trunk@2893 eaf649e6-6c13-0410-b194-84221022858e

--- hexen2/strl_fn.h
+++ hexen2/strl_fn.h
@@ -2,13 +2,15 @@
 	strl_fn.h
 	header file for BSD strlcat and strlcpy
 
-	$Id: strl_fn.h,v 1.3 2007-03-14 21:03:22 sezero Exp $
+	$Id: strl_fn.h,v 1.4 2007-09-22 16:11:06 sezero Exp $
 */
 
 #ifndef __STRLFUNCS_H
 #define __STRLFUNCS_H
 
-// do we have strlcpy and strlcat as native library functions?
+/* do we have strlcpy and strlcat as native library functions? */
+#undef	HAVE_STRLCPY
+#undef	HAVE_STRLCAT
 
 #if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__MACOSX__) || defined(__DragonFly__)
 
