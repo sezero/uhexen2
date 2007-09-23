96b68a17204e3c2a92f1a7fd0f743904d58ac629
Ozkan Sezer <sezeroz@gmail.com>
2007-09-23 18:50:11 +0000

    updated the beta release version/date (1.4.2-rc3, 2007-09-23.)
    git-svn-id: file:///svn/uhexen2/code/trunk@2898 eaf649e6-6c13-0410-b194-84221022858e

--- hexen2/quakedef.h
+++ hexen2/quakedef.h
@@ -2,7 +2,7 @@
 	quakedef.h
 	common definitions for client and server
 
-	$Id: quakedef.h,v 1.130 2007-09-22 16:47:52 sezero Exp $
+	$Id: quakedef.h,v 1.131 2007-09-23 18:50:10 sezero Exp $
 */
 
 #ifndef __QUAKEDEFS_H
@@ -14,7 +14,7 @@
 #define	HOT_VERSION_MAJ		1
 #define	HOT_VERSION_MID		4
 #define	HOT_VERSION_MIN		2
-#define	HOT_VERSION_REL_DATE	"2007-09-22"
+#define	HOT_VERSION_REL_DATE	"2007-09-23"
 #define	HOT_VERSION_BETA	1
 #define	HOT_VERSION_BETA_STR	"rc3"
 #define	HOT_VERSION_STR		STRINGIFY(HOT_VERSION_MAJ) "." STRINGIFY(HOT_VERSION_MID) "." STRINGIFY(HOT_VERSION_MIN)
