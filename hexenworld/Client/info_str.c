ee4c82f4f10392e051ecf594dd34bdbbcccdecf1
Ozkan Sezer <sezeroz@gmail.com>
2007-09-30 11:05:49 +0000

    hexenworld: a few fixes in info strings handling.
    git-svn-id: file:///svn/uhexen2/code/trunk@2919 eaf649e6-6c13-0410-b194-84221022858e

--- hexenworld/Client/info_str.c
+++ hexenworld/Client/info_str.c
@@ -2,7 +2,7 @@
 	info_str.c
 	Hexen2World info strings handling
 
-	$Id: info_str.c,v 1.6 2007-09-30 11:04:33 sezero Exp $
+	$Id: info_str.c,v 1.7 2007-09-30 11:05:49 sezero Exp $
 */
 
 #include "quakedef.h"
@@ -151,7 +151,7 @@ void Info_RemovePrefixedKeys (char *start, char prefix)
 
 }
 
-void Info_SetValueForStarKey (char *s, const char *key, const char *value, int maxsize)
+void Info_SetValueForStarKey (char *s, const char *key, const char *value, size_t maxsize)
 {
 	char	newvalue[1024], *v;
 	int		c;
@@ -173,17 +173,20 @@ void Info_SetValueForStarKey (char *s, const char *key, const char *value, int m
 		Con_Printf ("Keys and values must be < 64 characters.\n");
 		return;
 	}
+
 	Info_RemoveKey (s, key);
-	if (!value || !strlen(value))
+	if (!value || !*value)
 		return;
 
-	sprintf (newvalue, "\\%s\\%s", key, value);
-
-	if (strlen(newvalue) + strlen(s) > maxsize)
+	if (strlen(key) + 1 + strlen(value) + 1 + strlen(s) >= maxsize)
 	{
+	// >= maxsize, not > maxsize, for the null termination.
+	// +1 for each of key and value for the added \ characters
 		Con_Printf ("Info string length exceeded\n");
 		return;
 	}
+
+	sprintf (newvalue, "\\%s\\%s", key, value);
 
 	// only copy ascii values
 	s += strlen(s);
@@ -217,7 +220,7 @@ void Info_SetValueForStarKey (char *s, const char *key, const char *value, int m
 	*s = 0;
 }
 
-void Info_SetValueForKey (char *s, const char *key, const char *value, int maxsize)
+void Info_SetValueForKey (char *s, const char *key, const char *value, size_t maxsize)
 {
 	if (key[0] == '*')
 	{
--- hexenworld/Client/info_str.h
+++ hexenworld/Client/info_str.h
@@ -2,7 +2,7 @@
 	info_str.h
 	Hexen2World info strings handling
 
-	$Id: info_str.h,v 1.1 2007-02-11 13:08:30 sezero Exp $
+	$Id: info_str.h,v 1.2 2007-09-30 11:05:49 sezero Exp $
 */
 
 #ifndef __H2W_INFOSTR_H
@@ -15,8 +15,8 @@
 char *Info_ValueForKey (const char *s, const char *key);
 void Info_RemoveKey (char *s, const char *key);
 void Info_RemovePrefixedKeys (char *start, char prefix);
-void Info_SetValueForKey (char *s, const char *key, const char *value, int maxsize);
-void Info_SetValueForStarKey (char *s, const char *key, const char *value, int maxsize);
+void Info_SetValueForKey (char *s, const char *key, const char *value, size_t maxsize);
+void Info_SetValueForStarKey (char *s, const char *key, const char *value, size_t maxsize);
 void Info_Print (const char *s);
 
 #endif	/* __H2W_INFOSTR_H */
