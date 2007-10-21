f4a2b8ab65138e07a9a043eea62d55b4628b0fc6
Ozkan Sezer <sezeroz@gmail.com>
2007-10-21 15:35:10 +0000

    added conditionals around some input functions which are related to windowed environments.
    git-svn-id: file:///svn/uhexen2/code/trunk@2983 eaf649e6-6c13-0410-b194-84221022858e

--- hexen2/input.h
+++ hexen2/input.h
@@ -2,7 +2,7 @@
 	input.h
 	external (non-keyboard) input devices
 
-	$Id: input.h,v 1.8 2007-08-01 09:43:58 sezero Exp $
+	$Id: input.h,v 1.9 2007-10-21 15:35:10 sezero Exp $
 */
 
 #ifndef __HX2_INPUT_H
@@ -20,13 +20,21 @@ void IN_Move (usercmd_t *cmd);
 // add additional movement on top of the keyboard move cmd
 
 
+void IN_SendKeyEvents (void);
+
+
 // for window manager events
+#if !defined(PLATFORM_DOS)
 void IN_ShowMouse (void);
 void IN_DeactivateMouse (void);
 void IN_HideMouse (void);
 void IN_ActivateMouse (void);
-
-void IN_SendKeyEvents (void);
+#else	/* not used by dos driver */
+#define IN_ShowMouse()
+#define IN_DeactivateMouse()
+#define IN_HideMouse()
+#define IN_ActivateMouse()
+#endif	/* PLATFORM_DOS */
 
 #if defined(PLATFORM_WINDOWS)
 void IN_MouseEvent (int mstate);
@@ -35,7 +43,7 @@ void IN_Accumulate (void);
 void IN_SetQuakeMouseState (void);
 void IN_RestoreOriginalMouseState (void);
 void IN_UpdateClipCursor (void);
-#else	/* not used by SDL input driver */
+#else	/* not used by other drivers */
 #define IN_MouseEvent(E)
 #define IN_ClearStates()
 #define IN_Accumulate()
