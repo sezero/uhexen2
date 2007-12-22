2eeddcd5e4dba8bf41626afd021f9213310a19c4
Ozkan Sezer <sezeroz@gmail.com>
2007-12-22 19:01:26 +0000

    made sdl input driver to respond to the SDL_ACTIVEEVENT events: made the game to block / unblock sound upon focus loss / gain.
    git-svn-id: file:///svn/uhexen2/code/trunk@3099 eaf649e6-6c13-0410-b194-84221022858e

--- hexen2/in_sdl.c
+++ hexen2/in_sdl.c
@@ -2,7 +2,28 @@
 	in_sdl.c
 	SDL game input code
 
-	$Id: in_sdl.c,v 1.51 2007-12-21 12:00:10 sezero Exp $
+	$Id: in_sdl.c,v 1.52 2007-12-22 19:01:26 sezero Exp $
+
+	Copyright (C) 2001  contributors of the Anvil of Thyrion project
+	Copyright (C) 2005-2007  Steven Atkinson, O.Sezer
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	See the GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to:
+
+		Free Software Foundation, Inc.
+		51 Franklin St, Fifth Floor,
+		Boston, MA  02110-1301  USA
 */
 
 #include "sdl_inc.h"
@@ -452,6 +473,22 @@ void IN_SendKeyEvents (void)
 	{
 		switch (event.type)
 		{
+		case SDL_ACTIVEEVENT:
+			if (event.active.state & (SDL_APPINPUTFOCUS|SDL_APPACTIVE))
+			{
+				if (event.active.gain)
+				{
+				//	Sys_Printf("FOCUS GAIN\n");
+					S_UnblockSound();
+				}
+				else
+				{
+				//	Sys_Printf("FOCUS LOSS\n");
+					S_BlockSound();
+				}
+			}
+			break;
+
 		case SDL_KEYDOWN:
 			if ((event.key.keysym.sym == SDLK_RETURN) &&
 			    (event.key.keysym.mod & KMOD_ALT))
