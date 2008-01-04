00a100ce0599cbe2dc056b0ff4b2a4b81f41959e
Ozkan Sezer <sezeroz@gmail.com>
2008-01-04 19:36:17 +0000

    updated release date to 2008-01-04.
    git-svn-id: file:///svn/uhexen2/code/trunk@3109 eaf649e6-6c13-0410-b194-84221022858e

--- hexenworld/Client/quakedef.h
+++ hexenworld/Client/quakedef.h
@@ -2,7 +2,7 @@
 	quakedef.h
 	common definitions for client and server
 
-	$Id: quakedef.h,v 1.57 2007-12-24 18:45:35 sezero Exp $
+	$Id: quakedef.h,v 1.58 2008-01-04 19:36:17 sezero Exp $
 */
 
 #ifndef __QUAKEDEFS_H
@@ -14,7 +14,7 @@
 #define	HOT_VERSION_MAJ		1
 #define	HOT_VERSION_MID		4
 #define	HOT_VERSION_MIN		3
-#define	HOT_VERSION_REL_DATE	"2007-12-24"
+#define	HOT_VERSION_REL_DATE	"2008-01-04"
 #define	HOT_VERSION_BETA	1
 #define	HOT_VERSION_BETA_STR	"rc0"
 #define	HOT_VERSION_STR		STRINGIFY(HOT_VERSION_MAJ) "." STRINGIFY(HOT_VERSION_MID) "." STRINGIFY(HOT_VERSION_MIN)
