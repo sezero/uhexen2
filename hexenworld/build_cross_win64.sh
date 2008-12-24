9583e0509ece849958c77416f1dba6130fb65d8c
Ozkan Sezer <sezeroz@gmail.com>
2008-12-24 08:37:39 +0000

    support for win64 builds: adjusted the makefiles and added build_cross_win64 scripts.  at present, only cross compilation on linux was tested using the mingw-w64 beta builds from mingw-w64.sourceforge.net. there are some warnings but they can be fixed and the whole thing should be functional soon.
    git-svn-id: file:///svn/uhexen2/code/trunk@3408 eaf649e6-6c13-0410-b194-84221022858e

---
+++ hexenworld/build_cross_win64.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+UHEXEN2_TOP=..
+. $UHEXEN2_TOP/scripts/cross_defs_w64
+
+if test "$1" = "strip"; then
+	echo "Stripping all HexenWorld binaries"
+	$STRIPPER Master/hwmaster.exe	\
+		Server/hwsv.exe	\
+		Client/glhwcl.exe
+#		Client/hwcl.exe	\
+	exit 0
+fi
+
+HOST_OS=`uname|sed -e s/_.*//|tr '[:upper:]' '[:lower:]'`
+
+case "$HOST_OS" in
+linux)
+	MAKE_CMD=make
+	;;
+freebsd|openbsd|netbsd)
+	MAKE_CMD=gmake
+	;;
+*)
+	MAKE_CMD=make
+	;;
+esac
+
+if test "$1" = "clean"; then
+	$MAKE_CMD -s -C Client clean
+	$MAKE_CMD -s -C Master clean
+	$MAKE_CMD -s -C Server clean
+	exit 0
+fi
+
+echo "Building HexenWorld Server"
+$MAKE_CMD -C Server $* || exit 1
+
+echo "" && echo "Building HexenWorld Master Server"
+$MAKE_CMD -C Master $* || exit 1
+
+#echo "" && echo "Building HexenWorld Client (Software renderer)"
+#$MAKE_CMD -C Client $* hw || exit 1
+
+echo "" && echo "Building HexenWorld Client (OpenGL renderer)"
+$MAKE_CMD -C Client clean
+$MAKE_CMD -C Client $* glhw || exit 1
+
