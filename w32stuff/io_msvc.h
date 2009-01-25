8b1e7e06ecba7a7540f11bc1e59a826561658ccf
Ozkan Sezer <sezeroz@gmail.com>
2009-01-25 14:00:47 +0000

    updated io_msvc.h wrappers.
    git-svn-id: file:///svn/uhexen2/code/trunk@3493 eaf649e6-6c13-0410-b194-84221022858e

--- w32stuff/io_msvc.h
+++ w32stuff/io_msvc.h
@@ -1,9 +1,36 @@
-/* different MSVC names for <io.h> functions
+
+/* different MSVC names for <io.h> functions:
  * new MSVC versions spit deprecation warnings.
- * make SURE to include this AFTER io.h
+ * make SURE to include this AFTER you include
+ * io.h and/or conio.h
  */
 
-#if defined(_MSC_VER) && (_MSC_VER >= 1400)
+/* when it is windows-only code, we use the
+ * _prefixed versions, but not all compiler
+ * suits support all stuff, hence this header.
+ */
+
+/* MSVC compiler favors the _prefixed ones.
+ *
+ * MinGW is compatible with that, so it's OK
+ * with this, too.
+ *
+ * LCC has similar behavior: even if some of
+ * the functions are declared without prefix,
+ * it adds defines to help the situation most
+ * of the time.
+ *
+ * OpenWatcom 1.7 and older doesn't have most
+ * of the prefixed ones, but 1.8 does.
+ */
+
+#ifndef _CRT_QUIRKS
+#define _CRT_QUIRKS
+
+#if (defined(_WIN32) || defined(_WIN64))
+
+#if defined(_MSC_VER) || defined(__GNUC__)
+/* io.h stuff: */
 #  define read		_read
 #  define write		_write
 #  define open		_open
@@ -12,5 +39,27 @@
 #  define access	_access
 #  define rmdir		_rmdir
 #  define unlink	_unlink
-#endif	/* _MSC_VER */
+/* conio.h stuff: */
+#  define kbhit		_kbhit
+#  define getch		_getch
+#  define getche	_getche
+#  define putch		_putch
+#  define ungetch	_ungetch
+#endif	/* _MSC_VER, __GNUC__ */
+
+#if defined(__LCC__)
+#  define _rmdir	rmdir
+#endif	/* __LCC__ */
+
+#if defined(__WATCOMC__) && (__WATCOMC__ < 1280)
+#  define _read		read
+#  define _write	write
+#  define _unlink	unlink
+#  define _kbhit	kbhit
+#  define _putch	putch
+#  define _ungetch	ungetch
+#endif	/* __WATCOMC__ */
+
+#endif	/* _WIN32/_WIN64 */
+#endif	/* _CRT_QUIRKS */
 
