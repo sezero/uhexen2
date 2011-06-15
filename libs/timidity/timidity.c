80299e5e4ca520ecca0906268c8c57657d77465c
Ozkan Sezer <sezeroz@gmail.com>
2011-06-15 09:40:48 +0000

    libs/timidity/timidity.c (read_config_file): honor the return code       from recursive read_config_file() call for the "source" directive.       from timidity-0.2i bugfix patch found at       http://www.onicos.com/staff/iz/timidity/
    git-svn-id: file:///svn/uhexen2/code/trunk@4042 eaf649e6-6c13-0410-b194-84221022858e

--- libs/timidity/timidity.c
+++ libs/timidity/timidity.c
@@ -218,9 +218,14 @@ static int read_config_file(const char *name)
       }
       for (i=1; i<words; i++)
       {
+	int status;
 	rcf_count++;
-	read_config_file(w[i]);
+	status = read_config_file(w[i]);
 	rcf_count--;
+	if (status != 0) {
+	  fclose(fp);
+	  return status;
+	}
       }
     }
     else if (!strcmp(w[0], "default"))
