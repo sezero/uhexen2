e75496d1c721105320a5bd05f59936b41cd6b640
Ozkan Sezer <sezeroz@gmail.com>
2011-07-05 16:38:00 +0000

    * oslibs/dos/djstdint: Added new djstdint directory. * oslibs/dos/djstdint/stdint.h, inttypes.h: Added from djgpp v2.04 for   use with older djgpp versions.
    git-svn-id: file:///svn/uhexen2/code/trunk@4109 eaf649e6-6c13-0410-b194-84221022858e

---
+++ oslibs/dos/djstdint/inttypes.h
@@ -0,0 +1,204 @@
+/* Hacked from DJGPP v2.04 for use with older DJGPP versions */
+
+/* Copyright (C) 2003 DJ Delorie, see COPYING.DJ for details */
+/* Copyright (C) 2002 DJ Delorie, see COPYING.DJ for details */
+#ifndef __dj_inttypes__h_
+#define __dj_inttypes__h_
+
+#include <stdint.h>
+
+/* ANSI/ISO C99 says these should not be visible in C++ unless
+   explicitly requested.  */
+
+#if !defined(__cplusplus) || defined(__STDC_FORMAT_MACROS)
+
+#define PRId8       "hhd"
+#define PRId16      "hd"
+#define PRId32      "ld"
+#define PRId64      "lld"
+#define PRIdLEAST8  "hhd"
+#define PRIdLEAST16 "hd"
+#define PRIdLEAST32 "d"
+#define PRIdLEAST64 "lld"
+#define PRIdFAST8   "hhd"
+#define PRIdFAST16  "d"
+#define PRIdFAST32  "d"
+#define PRIdFAST64  "lld"
+#define PRIdMAX     "lld"
+#define PRIdPTR     "ld"
+
+#define PRIi8       "hhi"
+#define PRIi16      "hi"
+#define PRIi32      "li"
+#define PRIi64      "lli"
+#define PRIiLEAST8  "hhi"
+#define PRIiLEAST16 "hi"
+#define PRIiLEAST32 "i"
+#define PRIiLEAST64 "lli"
+#define PRIiFAST8   "hhi"
+#define PRIiFAST16  "i"
+#define PRIiFAST32  "i"
+#define PRIiFAST64  "lli"
+#define PRIiMAX     "lli"
+#define PRIiPTR     "li"
+
+#define PRIo8       "hho"
+#define PRIo16      "ho"
+#define PRIo32      "lo"
+#define PRIo64      "llo"
+#define PRIoLEAST8  "hho"
+#define PRIoLEAST16 "ho"
+#define PRIoLEAST32 "o"
+#define PRIoLEAST64 "llo"
+#define PRIoFAST8   "hho"
+#define PRIoFAST16  "o"
+#define PRIoFAST32  "o"
+#define PRIoFAST64  "llo"
+#define PRIoMAX     "llo"
+#define PRIoPTR     "lo"
+
+#define PRIu8       "hhu"
+#define PRIu16      "hu"
+#define PRIu32      "lu"
+#define PRIu64      "llu"
+#define PRIuLEAST8  "hhu"
+#define PRIuLEAST16 "hu"
+#define PRIuLEAST32 "u"
+#define PRIuLEAST64 "llu"
+#define PRIuFAST8   "hhu"
+#define PRIuFAST16  "u"
+#define PRIuFAST32  "u"
+#define PRIuFAST64  "llu"
+#define PRIuMAX     "llu"
+#define PRIuPTR     "lu"
+
+#define PRIx8       "hhx"
+#define PRIx16      "hx"
+#define PRIx32      "lx"
+#define PRIx64      "llx"
+#define PRIxLEAST8  "hhx"
+#define PRIxLEAST16 "hx"
+#define PRIxLEAST32 "x"
+#define PRIxLEAST64 "llx"
+#define PRIxFAST8   "hhx"
+#define PRIxFAST16  "x"
+#define PRIxFAST32  "x"
+#define PRIxFAST64  "llx"
+#define PRIxMAX     "llx"
+#define PRIxPTR     "lx"
+
+#define PRIX8       "hhX"
+#define PRIX16      "hX"
+#define PRIX32      "lX"
+#define PRIX64      "llX"
+#define PRIXLEAST8  "hhX"
+#define PRIXLEAST16 "hX"
+#define PRIXLEAST32 "X"
+#define PRIXLEAST64 "llX"
+#define PRIXFAST8   "hhX"
+#define PRIXFAST16  "X"
+#define PRIXFAST32  "X"
+#define PRIXFAST64  "llX"
+#define PRIXMAX     "llX"
+#define PRIXPTR     "lX"
+
+#define SCNd8       "hhd"
+#define SCNd16      "hd"
+#define SCNd32      "ld"
+#define SCNd64      "lld"
+#define SCNdLEAST8  "hhd"
+#define SCNdLEAST16 "hd"
+#define SCNdLEAST32 "d"
+#define SCNdLEAST64 "lld"
+#define SCNdFAST8   "hhd"
+#define SCNdFAST16  "d"
+#define SCNdFAST32  "d"
+#define SCNdFAST64  "lld"
+#define SCNdMAX     "lld"
+#define SCNdPTR     "ld"
+
+#define SCNi8       "hhi"
+#define SCNi16      "hi"
+#define SCNi32      "li"
+#define SCNi64      "lli"
+#define SCNiLEAST8  "hhi"
+#define SCNiLEAST16 "hi"
+#define SCNiLEAST32 "i"
+#define SCNiLEAST64 "lli"
+#define SCNiFAST8   "hhi"
+#define SCNiFAST16  "i"
+#define SCNiFAST32  "i"
+#define SCNiFAST64  "lli"
+#define SCNiMAX     "lli"
+#define SCNiPTR     "li"
+
+#define SCNo8       "hho"
+#define SCNo16      "ho"
+#define SCNo32      "lo"
+#define SCNo64      "llo"
+#define SCNoLEAST8  "hho"
+#define SCNoLEAST16 "ho"
+#define SCNoLEAST32 "o"
+#define SCNoLEAST64 "llo"
+#define SCNoFAST8   "hho"
+#define SCNoFAST16  "o"
+#define SCNoFAST32  "o"
+#define SCNoFAST64  "llo"
+#define SCNoMAX     "llo"
+#define SCNoPTR     "lo"
+
+#define SCNu8       "hhu"
+#define SCNu16      "hu"
+#define SCNu32      "lu"
+#define SCNu64      "llu"
+#define SCNuLEAST8  "hhu"
+#define SCNuLEAST16 "hu"
+#define SCNuLEAST32 "u"
+#define SCNuLEAST64 "llu"
+#define SCNuFAST8   "hhu"
+#define SCNuFAST16  "u"
+#define SCNuFAST32  "u"
+#define SCNuFAST64  "llu"
+#define SCNuMAX     "llu"
+#define SCNuPTR     "lu"
+
+#define SCNx8       "hhx"
+#define SCNx16      "hx"
+#define SCNx32      "lx"
+#define SCNx64      "llx"
+#define SCNxLEAST8  "hhx"
+#define SCNxLEAST16 "hx"
+#define SCNxLEAST32 "x"
+#define SCNxLEAST64 "llx"
+#define SCNxFAST8   "hhx"
+#define SCNxFAST16  "x"
+#define SCNxFAST32  "x"
+#define SCNxFAST64  "llx"
+#define SCNxMAX     "llx"
+#define SCNxPTR     "lx"
+
+#endif /* !__cplusplus || __STDC_FORMAT_MACROS */
+
+/* we don't need the functions below.
+ * when/if we do, enable the prototypes and implement them... */
+/*
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+  intmax_t quot;
+  intmax_t rem;
+} imaxdiv_t;
+
+intmax_t imaxabs (intmax_t _j);
+imaxdiv_t imaxdiv (intmax_t _numer, intmax_t _denom);
+intmax_t strtoimax (const char *_nptr, char **_endptr, int _base);
+uintmax_t strtoumax (const char *_nptr, char **_endptr, int _base);
+
+#ifdef __cplusplus
+}
+#endif
+*/
+
+#endif /* __dj_inttypes__h_ */
---
+++ oslibs/dos/djstdint/stdint.h
@@ -0,0 +1,135 @@
+/* Hacked from DJGPP v2.04 for use with older DJGPP versions */
+
+/* Copyright (C) 2003 DJ Delorie, see COPYING.DJ for details */
+/* Copyright (C) 2002 DJ Delorie, see COPYING.DJ for details */
+/* Copyright (C) 2001 DJ Delorie, see COPYING.DJ for details */
+#ifndef __dj_stdint__h_
+#define __dj_stdint__h_
+
+typedef signed char int_least8_t;
+typedef unsigned char uint_least8_t;
+typedef signed char int_fast8_t;
+typedef unsigned char uint_fast8_t;
+typedef signed char int8_t;
+typedef unsigned char uint8_t;
+
+typedef signed short int int_least16_t;
+typedef unsigned short int uint_least16_t;
+typedef signed int int_fast16_t;
+typedef unsigned int uint_fast16_t;
+typedef signed short int int16_t;
+typedef unsigned short int uint16_t;
+
+typedef signed int int_least32_t;
+typedef unsigned int uint_least32_t;
+typedef signed int int_fast32_t;
+typedef unsigned int uint_fast32_t;
+typedef signed long int32_t;
+typedef unsigned long uint32_t;
+
+__extension__ typedef signed long long int_least64_t;
+__extension__ typedef unsigned long long uint_least64_t;
+__extension__ typedef signed long long int_fast64_t;
+__extension__ typedef unsigned long long uint_fast64_t;
+__extension__ typedef signed long long int64_t;
+__extension__ typedef unsigned long long uint64_t;
+
+typedef long int intptr_t;
+typedef unsigned long uintptr_t;
+
+__extension__ typedef signed long long intmax_t;
+__extension__ typedef unsigned long long uintmax_t;
+
+/* ANSI/ISO C99 says these should not be visible in C++ unless
+   explicitly requested.  */
+
+#if !defined(__cplusplus) || defined(__STDC_LIMIT_MACROS)
+
+#define INT_LEAST8_MAX	 127
+#define UINT_LEAST8_MAX	 255
+#define INT_FAST8_MAX	 127
+#define UINT_FAST8_MAX	 255
+#define INT8_MAX	 127
+#define UINT8_MAX	 255 
+#define INT_LEAST8_MIN	 (-128)
+#define INT_FAST8_MIN	 (-128)
+#define INT8_MIN	 (-128)
+
+#define INT_LEAST16_MAX	 32767
+#define UINT_LEAST16_MAX 65535
+#define INT_FAST16_MAX	 2147483647
+#define UINT_FAST16_MAX	 4294967295U
+#define INT16_MAX	 32767
+#define UINT16_MAX	 65535
+#define INT_LEAST16_MIN	 (-32768)
+#define INT_FAST16_MIN	 (-2147483647-1)
+#define INT16_MIN	 (-32768)
+
+#define INT_LEAST32_MAX	 2147483647
+#define UINT_LEAST32_MAX 4294967295U
+#define INT_FAST32_MAX	 2147483647
+#define UINT_FAST32_MAX	 4294967295U
+#define INT32_MAX	 2147483647L
+#define UINT32_MAX	 4294967295UL
+#define INT_LEAST32_MIN	 (-2147483647-1)
+#define INT_FAST32_MIN	 (-2147483647-1)
+#define INT32_MIN	 (-2147483647L-1)
+
+#define INT_LEAST64_MAX	 9223372036854775807LL
+#define UINT_LEAST64_MAX 18446744073709551615ULL
+#define INT_FAST64_MAX	 9223372036854775807LL
+#define UINT_FAST64_MAX	 18446744073709551615ULL
+#define INT64_MAX	 9223372036854775807LL
+#define UINT64_MAX	 18446744073709551615ULL
+#define INT_LEAST64_MIN	 (-9223372036854775807LL-1LL)
+#define INT_FAST64_MIN	 (-9223372036854775807LL-1LL)
+#define INT64_MIN	 (-9223372036854775807LL-1LL)
+
+#define INTPTR_MAX	2147483647L
+#define INTPTR_MIN	(-2147483647L-1L)
+#define UINTPTR_MAX	0xffffffffUL
+
+#define INTMAX_MAX	9223372036854775807LL
+#define UINTMAX_MAX	18446744073709551615ULL
+#define INTMAX_MIN	(-9223372036854775807LL-1LL)
+
+#define PTRDIFF_MAX	2147483647
+#define PTRDIFF_MIN	(-2147483647-1)
+#define SIG_ATOMIC_MAX	2147483647
+#define SIG_ATOMIC_MIN	(-2147483647-1)
+#define SIZE_MAX	4294967295U
+
+  /* These are defined by limits.h, so make them conditional.  */
+#ifndef WCHAR_MAX
+#define WCHAR_MAX	65535
+#endif
+#ifndef WCHAR_MIN
+#define WCHAR_MIN	0
+#endif
+#ifndef WINT_MAX
+#define WINT_MAX	2147483647
+#endif
+#ifndef WINT_MIN
+#define WINT_MIN	(-2147483647-1)
+#endif
+
+#endif /* !__cplusplus || __STDC_LIMIT_MACROS */
+
+
+#if !defined(__cplusplus) || defined(__STDC_CONSTANT_MACROS)
+
+#define INT8_C(x)	((int8_t)x)
+#define UINT8_C(x)	((uint8_t)x ## U)
+#define INT16_C(x)	((int16_t)x)
+#define UINT16_C(x)	((uint16_t)x ## U)
+#define INT32_C(x)	x ## L
+#define UINT32_C(x)	x ## UL
+#define INT64_C(x)	x ## LL
+#define UINT64_C(x)	x ## ULL
+
+#define INTMAX_C(x)	x ## LL
+#define UINTMAX_C(x)	x ## ULL
+
+#endif /* !__cplusplus || __STDC_CONSTANT_MACROS */
+
+#endif /* !__dj_stdint__h_ */
