d0159f008fd0de47334cf4bb92ba349e0db53cdf
Ozkan Sezer <sezeroz@gmail.com>
2011-12-24 12:20:27 +0000

    cfgfile: changed to use FS_ functions, patch from Sander van Dijk. updated copyright years.
    git-svn-id: file:///svn/uhexen2/code/trunk@4405 eaf649e6-6c13-0410-b194-84221022858e

--- engine/h2shared/cfgfile.c
+++ engine/h2shared/cfgfile.c
@@ -4,7 +4,7 @@
 
 	$Id$
 
-	Copyright (C) 2008-2010  O.Sezer <sezero@users.sourceforge.net>
+	Copyright (C) 2008-2011  O.Sezer <sezero@users.sourceforge.net>
 
 	This program is free software; you can redistribute it and/or
 	modify it under the terms of the GNU General Public License
@@ -28,7 +28,7 @@
 #include "quakedef.h"
 
 
-static FILE			*cfg_file;
+static fshandle_t		*cfg_file;
 
 /*
 ===================
@@ -54,8 +54,7 @@ void CFG_ReadCvars (const char **vars, int num_vars)
 	do {
 		i = 0;
 		memset (buff, 0, sizeof(buff));
-		fgets(buff, sizeof(buff), cfg_file);
-		if (!feof(cfg_file))
+		if (FS_fgets(buff, sizeof(buff), cfg_file))
 		{
 			// we expect a line in the format that Cvar_WriteVariables
 			// writes to the config file. although I'm trying to be as
@@ -112,9 +111,9 @@ void CFG_ReadCvars (const char **vars, int num_vars)
 		if (j == num_vars)
 			break;
 
-	} while (!feof(cfg_file));
+	} while (!FS_feof(cfg_file));
 
-	fseek (cfg_file, 0, SEEK_SET);
+	FS_fseek (cfg_file, 0, SEEK_SET);
 }
 
 void CFG_ReadCvarOverrides (const char **vars, int num_vars)
@@ -143,17 +142,32 @@ void CFG_CloseConfig (void)
 {
 	if (cfg_file)
 	{
-		fclose (cfg_file);
+		FS_fclose(cfg_file);
+		Z_Free(cfg_file);
 		cfg_file = NULL;
 	}
 }
 
 int CFG_OpenConfig (const char *cfg_name)
 {
+	FILE *file;
+	size_t length;
+	qboolean pak;
+
 	CFG_CloseConfig ();
-	FS_OpenFile (cfg_name, &cfg_file, NULL);
-	if (!cfg_file)
+
+	length = FS_OpenFile (cfg_name, &file, NULL);
+	pak = file_from_pak;
+	if (length == (size_t)-1)
 		return -1;
+
+	cfg_file = (fshandle_t *) Z_Malloc(sizeof(fshandle_t), Z_MAINZONE);
+	cfg_file->file = file;
+	cfg_file->start = ftell(file);
+	cfg_file->pos = 0;
+	cfg_file->length = (long)length;
+	cfg_file->pak = file_from_pak;
+
 	return 0;
 }
 
--- engine/h2shared/cfgfile.h
+++ engine/h2shared/cfgfile.h
@@ -4,7 +4,7 @@
 
 	$Id$
 
-	Copyright (C) 2008-2010  O.Sezer <sezero@users.sourceforge.net>
+	Copyright (C) 2008-2011  O.Sezer <sezero@users.sourceforge.net>
 
 	This program is free software; you can redistribute it and/or
 	modify it under the terms of the GNU General Public License
