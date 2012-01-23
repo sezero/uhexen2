6217697dcb6badaa13ace86c0a0b904253536949
Ozkan Sezer <sezeroz@gmail.com>
2012-01-23 00:01:34 +0000

    * vid_dos.c (VID_Init): Added missing safemode check alongside the   -stdvid command line argument check. * updated beta date stamp to 2012-01-23.
    git-svn-id: file:///svn/uhexen2/code/trunk@4486 eaf649e6-6c13-0410-b194-84221022858e

--- engine/h2shared/vid_dos.c
+++ engine/h2shared/vid_dos.c
@@ -117,7 +117,7 @@ void    VID_Init (unsigned char *palette)
 // set up the mode list; note that later inits link in their modes ahead of
 // earlier ones, so the standard VGA modes are always first in the list. This
 // is important because mode 0 must always be VGA mode 0x13
-	if (!COM_CheckParm ("-stdvid"))
+	if (!safemode && !COM_CheckParm("-stdvid"))
 		VID_InitExtra ();
 	VGA_Init ();
 
