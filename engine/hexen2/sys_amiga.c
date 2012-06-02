19b3411a0e8d91ab55d0b655701592cb8e71704a
Ozkan Sezer <sezeroz@gmail.com>
2012-06-02 08:23:19 +0000

    * engine/hexen2/sys_amiga.c: Support for Amiga (AROS), many thanks   to Szilard Biro. * engine/hexen2/Makefile, scripts/makefile.inc: Add adjustments for   aros. * updated version date stamp to 2012-06-02.
    git-svn-id: file:///svn/uhexen2/code/trunk@4720 eaf649e6-6c13-0410-b194-84221022858e

---
+++ engine/hexen2/sys_amiga.c
@@ -0,0 +1,805 @@
+/*
+	sys_amiga.c
+	Amiga system interface code
+
+	$Id$
+*/
+
+#include "quakedef.h"
+#include "debuglog.h"
+
+#include <proto/exec.h>
+#include <proto/dos.h>
+
+#include <proto/intuition.h>
+#include <proto/iffparse.h>
+#include <datatypes/textclass.h>
+
+#include <devices/timer.h>
+#include <proto/timer.h>
+
+#include <time.h>
+#if defined(SDLQUAKE)
+#include "sdl_inc.h"
+#endif	/* SDLQUAKE */
+
+
+// heapsize: minimum 16mb, standart 32 mb, max is 96 mb.
+// -heapsize argument will abide by these min/max settings
+// unless the -forcemem argument is used
+#define MIN_MEM_ALLOC	0x1000000
+#define STD_MEM_ALLOC	0x2000000
+#define MAX_MEM_ALLOC	0x6000000
+
+cvar_t		sys_nostdout = {"sys_nostdout", "0", CVAR_NONE};
+cvar_t		sys_throttle = {"sys_throttle", "0.02", CVAR_ARCHIVE};
+
+qboolean		isDedicated;
+static double		starttime;
+static qboolean		first = true;
+
+struct timerequest	*timerio;
+struct MsgPort		*timerport;
+struct Device		*TimerBase;
+
+
+/*
+===============================================================================
+
+FILE IO
+
+===============================================================================
+*/
+
+int Sys_mkdir(const char *path, qboolean crash)
+{
+	BPTR lock = CreateDir((const STRPTR) path);
+
+	if (lock)
+	{
+		UnLock(lock);
+		return 0;
+	}
+
+	if (IoErr() == ERROR_OBJECT_EXISTS)
+		return 0;
+
+	if (crash)
+		Sys_Error("Unable to create directory %s", path);
+	return -1;
+}
+
+int Sys_rmdir (const char *path)
+{
+	if (DeleteFile((const STRPTR) path) != 0)
+		return 0;
+	return -1;
+}
+
+int Sys_unlink (const char *path)
+{
+	if (DeleteFile((const STRPTR) path) != 0)
+		return 0;
+	return -1;
+}
+
+int Sys_rename (const char *oldp, const char *newp)
+{
+	if (Rename((const STRPTR) oldp, (const STRPTR) newp) != 0)
+		return 0;
+	return -1;
+}
+
+long Sys_filesize (const char *path)
+{
+	long size = -1;
+	BPTR fh = Open((const STRPTR) path, MODE_OLDFILE);
+	if (fh != NULL)
+	{
+		struct FileInfoBlock *fib = (struct FileInfoBlock*)
+					AllocDosObject(DOS_FIB, NULL);
+		if (fib != NULL)
+		{
+			if (ExamineFH(fh, fib))
+				size = fib->fib_Size;
+			FreeDosObject(DOS_FIB, fib);
+		}
+		Close(fh);
+	}
+	return size;
+}
+
+int Sys_FileType (const char *path)
+{
+	long type = FS_ENT_NONE;
+	BPTR fh = Open((const STRPTR) path, MODE_OLDFILE);
+	if (fh != NULL)
+	{
+		struct FileInfoBlock *fib = (struct FileInfoBlock*)
+					AllocDosObject(DOS_FIB, NULL);
+		if (fib != NULL)
+		{
+			if (ExamineFH(fh, fib))
+			{
+				if (fib->fib_DirEntryType >= 0)
+					type = FS_ENT_DIRECTORY;
+				else	type = FS_ENT_FILE;
+			}
+			FreeDosObject(DOS_FIB, fib);
+		}
+		Close(fh);
+	}
+	return type;
+}
+
+#define	COPY_READ_BUFSIZE		8192	/* BUFSIZ */
+int Sys_CopyFile (const char *frompath, const char *topath)
+{
+	char buf[COPY_READ_BUFSIZE];
+	long remaining = 0;
+	long count;
+	BPTR in, out;
+	struct FileInfoBlock *fib;
+
+	in = Open((const STRPTR) frompath, MODE_OLDFILE);
+	if (!in)
+	{
+		Con_Printf ("%s: unable to open %s\n", frompath, __thisfunc__);
+		return 1;
+	}
+	fib = (struct FileInfoBlock*) AllocDosObject(DOS_FIB, NULL);
+	if (fib != NULL)
+	{
+		if (ExamineFH(in, fib))
+			remaining = fib->fib_Size;
+		FreeDosObject(DOS_FIB, fib);
+	}
+	else
+	{
+		Con_Printf ("%s: can't allocate the FileInfoBlock %s\n", frompath, __thisfunc__);
+		return 1;
+	}
+	out = Open((const STRPTR) topath, MODE_NEWFILE);
+	if (!out)
+	{
+		Con_Printf ("%s: unable to open %s\n", topath, __thisfunc__);
+		Close(in);
+		return 1;
+	}
+
+	memset (buf, 0, sizeof(buf));
+	while (remaining)
+	{
+		if (remaining < sizeof(buf))
+			count = remaining;
+		else
+			count = sizeof(buf);
+
+		FRead(in, buf, 1, count);
+		if (IoErr())
+			break;
+
+		FWrite(out, buf, 1, count);
+		if (IoErr())
+			break;
+
+		remaining -= count;
+	}
+
+	Close(in);
+	Close(out);
+
+	if (remaining != 0)
+		return 1;
+
+	return 0;
+}
+
+/*
+=================================================
+simplified findfirst/findnext implementation:
+Sys_FindFirstFile and Sys_FindNextFile return
+filenames only, not a dirent struct. this is
+what we presently need in this engine.
+=================================================
+*/
+
+#define PATH_SIZE 1024
+static struct AnchorPath *apath;
+static struct FileInfoBlock *find_fib;
+static BPTR oldcurrentdir;
+
+const char *Sys_FindFirstFile (const char *path, const char *pattern)
+{
+	if (apath)
+		Sys_Error ("Sys_FindFirst without FindClose");
+
+	apath = AllocMem (sizeof(struct AnchorPath) + PATH_SIZE, MEMF_CLEAR);
+	if (!apath)
+		return NULL;
+
+	apath->ap_Strlen = PATH_SIZE;
+	apath->ap_BreakBits = 0;
+	apath->ap_Flags = APB_DOWILD | !APB_DODIR;
+	oldcurrentdir = CurrentDir(Lock((const STRPTR) path, SHARED_LOCK));
+	if (MatchFirst((const STRPTR) pattern, apath) == 0)
+	{
+		find_fib = &(apath->ap_Info);
+		return (const char *) find_fib->fib_FileName;
+	}
+	return NULL;
+}
+
+const char *Sys_FindNextFile (void)
+{
+	if (!apath)
+		return NULL;
+
+	if (MatchNext(apath) == 0)
+	{
+		find_fib = &(apath->ap_Info);
+		return (const char *) find_fib->fib_FileName;
+	}
+
+	return NULL;
+}
+
+void Sys_FindClose (void)
+{
+	if (apath == NULL)
+		return;
+	MatchEnd(apath);
+	FreeMem(apath, sizeof(struct AnchorPath) + PATH_SIZE);
+	CurrentDir(oldcurrentdir);
+	oldcurrentdir = NULL;
+	apath = NULL;
+	find_fib = NULL;
+}
+
+/*
+===============================================================================
+
+SYSTEM IO
+
+===============================================================================
+*/
+
+/*
+================
+Sys_MakeCodeWriteable
+================
+*/
+#if id386 && !defined(GLQUAKE)
+void Sys_MakeCodeWriteable (unsigned long startaddr, unsigned long length)
+{
+/* Not needed on Amiga. */
+}
+#endif	/* id386, !GLQUAKE */
+
+
+/*
+================
+Sys_Init
+================
+*/
+static void Sys_Init (void)
+{
+/*	Sys_SetFPCW();*/
+
+	if ((timerport = CreateMsgPort()))
+	{
+		if ((timerio = CreateIORequest(timerport, sizeof(timerio))))
+		{
+			if (OpenDevice((STRPTR) TIMERNAME, UNIT_MICROHZ,
+					(struct IORequest *) timerio, 0) == 0)
+			{
+				TimerBase = timerio->tr_node.io_Device;
+			}
+			else
+			{
+				DeleteIORequest(timerio);
+				DeleteMsgPort(timerport);
+			}
+		}
+		else
+		{
+			DeleteMsgPort(timerport);
+		}
+	}
+	if (!TimerBase)
+		Sys_Error("Can't open timer.device");
+}
+
+void Sys_ErrorMessage(const char *string)
+{
+	struct EasyStruct es;
+
+	es.es_StructSize = sizeof(es);
+	es.es_Flags = 0;
+	es.es_Title = (STRPTR) ENGINE_NAME " error";
+	es.es_TextFormat = (STRPTR) string;
+	es.es_GadgetFormat = (STRPTR) "Quit";
+
+	EasyRequest(0, &es, 0, 0);
+}
+
+#define ERROR_PREFIX	"\nFATAL ERROR: "
+void Sys_Error (const char *error, ...)
+{
+	va_list		argptr;
+	char		text[MAX_PRINTMSG];
+	const char	text2[] = ERROR_PREFIX;
+	const unsigned char	*p;
+
+	va_start (argptr, error);
+	q_vsnprintf (text, sizeof(text), error, argptr);
+	va_end (argptr);
+
+	if (con_debuglog)
+	{
+		LOG_Print (ERROR_PREFIX);
+		LOG_Print (text);
+		LOG_Print ("\n\n");
+	}
+
+	Host_Shutdown ();
+
+	for (p = (const unsigned char *) text2; *p; p++)
+		putc (*p, stderr);
+	for (p = (const unsigned char *) text ; *p; p++)
+		putc (*p, stderr);
+	putc ('\n', stderr);
+	putc ('\n', stderr);
+	if (!isDedicated)
+		Sys_ErrorMessage (text);
+
+	exit (1);
+}
+
+void Sys_PrintTerm (const char *msgtxt)
+{
+	const unsigned char	*p;
+
+	if (sys_nostdout.integer)
+		return;
+
+	for (p = (const unsigned char *) msgtxt; *p; p++)
+		putc (*p, stdout);
+}
+
+void Sys_Quit (void)
+{
+	Host_Shutdown();
+
+	if (TimerBase)
+	{
+		/*
+		if (!CheckIO((struct IORequest *) timerio)
+		{
+			AbortIO((struct IORequest *) timerio);
+			WaitIO((struct IORequest *) timerio);
+		}
+		*/
+		WaitIO((struct IORequest *) timerio);
+		CloseDevice((struct IORequest *) timerio);
+		DeleteIORequest((struct IORequest *) timerio);
+		DeleteMsgPort(timerport);
+		TimerBase = NULL;
+	}
+
+	exit (0);
+}
+
+
+/*
+================
+Sys_DoubleTime
+================
+*/
+double Sys_DoubleTime (void)
+{
+	struct timeval	tp;
+	double		now;
+
+	GetSysTime(&tp);
+
+	now = tp.tv_sec + tp.tv_usec / 1e6;
+
+	if (first)
+	{
+		first = false;
+		starttime = now;
+		return 0.0;
+	}
+
+	return now - starttime;
+}
+
+char *Sys_DateTimeString (char *buf)
+{
+	static char strbuf[24];
+	time_t t;
+	struct tm *l;
+	int val;
+
+	if (!buf) buf = strbuf;
+
+	t = time(NULL);
+	l = localtime(&t);
+
+	val = l->tm_mon + 1;	/* tm_mon: months since January [0,11] */
+	buf[0] = val / 10 + '0';
+	buf[1] = val % 10 + '0';
+	buf[2] = '/';
+	val = l->tm_mday;
+	buf[3] = val / 10 + '0';
+	buf[4] = val % 10 + '0';
+	buf[5] = '/';
+	val = l->tm_year / 100 + 19;	/* tm_year: #years since 1900. */
+	buf[6] = val / 10 + '0';
+	buf[7] = val % 10 + '0';
+	val = l->tm_year % 100;
+	buf[8] = val / 10 + '0';
+	buf[9] = val % 10 + '0';
+
+	buf[10] = ' ';
+
+	val = l->tm_hour;
+	buf[11] = val / 10 + '0';
+	buf[12] = val % 10 + '0';
+	buf[13] = ':';
+	val = l->tm_min;
+	buf[14] = val / 10 + '0';
+	buf[15] = val % 10 + '0';
+	buf[16] = ':';
+	val = l->tm_sec;
+	buf[17] = val / 10 + '0';
+	buf[18] = val % 10 + '0';
+
+	buf[19] = '\0';
+
+	return buf;
+}
+
+
+/*
+================
+Sys_ConsoleInput
+================
+*/
+const char *Sys_ConsoleInput (void)
+{
+	static char	con_text[256];
+	static int	textlen;
+	char		c;
+
+	while (WaitForChar(Input(),10))
+	{
+		Read (0, &c, 1);
+		if (c == '\n' || c == '\r')
+		{
+			con_text[textlen] = '\0';
+			textlen = 0;
+			return con_text;
+		}
+		else if (c == 8)
+		{
+			if (textlen)
+			{
+				textlen--;
+				con_text[textlen] = '\0';
+			}
+			continue;
+		}
+		con_text[textlen] = c;
+		textlen++;
+		if (textlen < (int) sizeof(con_text))
+			con_text[textlen] = '\0';
+		else
+		{
+		// buffer is full
+			textlen = 0;
+			con_text[0] = '\0';
+			Sys_PrintTerm("\nConsole input too long!\n");
+			break;
+		}
+	}
+
+	return NULL;
+}
+
+void Sys_Sleep (unsigned long msecs)
+{
+	timerio->tr_node.io_Command = TR_ADDREQUEST;
+//	timerio->tr_time.tv_secs = (msecs * 1000) / 1000000; // i'm lazy...
+	timerio->tr_time.tv_secs = msecs / 1000; // not anymore
+	timerio->tr_time.tv_micro = (msecs * 1000) % 1000000;
+	SendIO((struct IORequest *) timerio);
+	WaitIO((struct IORequest *) timerio);
+}
+
+void Sys_SendKeyEvents (void)
+{
+	IN_SendKeyEvents();
+}
+
+/* This never gets called on AROS due to some SDL bug in the key handling.
+   I'll leave it in for future releases. */
+static char chunk_buffer[1024];
+
+char *Sys_GetClipboardData (void)
+{
+	struct IFFHandle *IFFHandle;
+	struct ContextNode *cn;
+	ULONG error, readbytes = 0;
+
+	if ((IFFHandle = AllocIFF())) {
+	    if ((IFFHandle->iff_Stream = (ULONG)OpenClipboard(0))) {
+		InitIFFasClip(IFFHandle);
+		if (!OpenIFF(IFFHandle, IFFF_READ)) {
+		    if (!StopChunk(IFFHandle, ID_FTXT, ID_CHRS)) {
+			if (!(error = ParseIFF(IFFHandle, IFFPARSE_SCAN))) {
+			    cn = CurrentChunk(IFFHandle);
+			    if (cn && (cn->cn_Type == ID_FTXT) &&
+					(cn->cn_ID == ID_CHRS)) {
+				readbytes = ReadChunkBytes(IFFHandle,
+							   chunk_buffer, 1024);
+			    }
+			}
+		    }
+		    CloseIFF(IFFHandle);
+		}
+		CloseClipboard((struct ClipboardHandle *) IFFHandle->iff_Stream);
+	    }
+	    FreeIFF(IFFHandle);
+	}
+
+	if (readbytes > 0)
+	    return chunk_buffer;
+	return NULL;
+}
+
+static int Sys_GetBasedir (char *argv0, char *dst, size_t dstsize)
+{
+	struct Task *self;
+	BPTR lock;
+
+	self = FindTask(NULL);
+	lock = ((struct Process *) self)->pr_CurrentDir;
+
+	if (NameFromLock(lock, (STRPTR) dst, dstsize) != 0)
+		return 0;
+	return -1;
+}
+
+static void Sys_CheckSDL (void)
+{
+#if defined(SDLQUAKE)
+	const SDL_version *sdl_version;
+
+	sdl_version = SDL_Linked_Version();
+	Sys_Printf("Found SDL version %i.%i.%i\n",sdl_version->major,sdl_version->minor,sdl_version->patch);
+	if (SDL_VERSIONNUM(sdl_version->major,sdl_version->minor,sdl_version->patch) < SDL_REQUIREDVERSION)
+	{	/*reject running under SDL versions older than what is stated in sdl_inc.h */
+		Sys_Error("You need at least v%d.%d.%d of SDL to run this game.", SDL_MIN_X,SDL_MIN_Y,SDL_MIN_Z);
+	}
+# if defined(SDL_NEW_VERSION_REJECT)
+	if (SDL_VERSIONNUM(sdl_version->major,sdl_version->minor,sdl_version->patch) >= SDL_NEW_VERSION_REJECT)
+	{	/*reject running under SDL versions newer than what is stated in sdl_inc.h */
+		Sys_Error("Your version of SDL library is incompatible with me.\n"
+			  "You need a library version in the line of %d.%d.%d\n", SDL_MIN_X,SDL_MIN_Y,SDL_MIN_Z);
+	}
+# endif /* SDL_NEW_VERSION_REJECT */
+#endif	/* SDLQUAKE */
+}
+
+static void PrintVersion (void)
+{
+#if HOT_VERSION_BETA
+	Sys_Printf ("Hammer of Thyrion, %s-%s (%s) pre-release\n", HOT_VERSION_STR, HOT_VERSION_BETA_STR, HOT_VERSION_REL_DATE);
+#else
+	Sys_Printf ("Hammer of Thyrion, release %s (%s)\n", HOT_VERSION_STR, HOT_VERSION_REL_DATE);
+#endif
+	Sys_Printf ("running on %s engine %4.2f (%s)\n", ENGINE_NAME, ENGINE_VERSION, PLATFORM_STRING);
+	Sys_Printf ("More info / sending bug reports:  http://uhexen2.sourceforge.net\n");
+}
+
+#include "snd_sys.h"
+static const char *help_strings[] = {
+	"     [-v | --version]        Display version information",
+#ifndef DEMOBUILD
+#   if defined(H2MP)
+	"     [-noportals]            Disable the mission pack support",
+#   else
+	"     [-portals | -h2mp ]     Run the Portal of Praevus mission pack",
+#   endif
+#endif
+	"     [-w | -window ]         Run the game windowed",
+	"     [-f | -fullscreen]      Run the game fullscreen",
+	"     [-width X [-height Y]]  Select screen size",
+#ifdef GLQUAKE
+	"     [-bpp]                  Depth for GL fullscreen mode",
+	"     [-g | -gllibrary]       Select 3D rendering library",
+	"     [-fsaa N]               Enable N sample antialiasing",
+	"     [-paltex]               Enable 8-bit textures",
+	"     [-nomtex]               Disable multitexture detection/usage",
+#endif
+#if !defined(_NO_SOUND)
+#if SOUND_NUMDRIVERS
+	"     [-s | -nosound]         Run the game without sound",
+#endif
+#if (SOUND_NUMDRIVERS > 1)
+#if HAVE_SDL_SOUND
+	"     [-sndsdl]               Use SDL sound",
+#endif
+#endif	/*  SOUND_NUMDRIVERS */
+#endif	/* _NO_SOUND */
+	"     [-nomouse]              Disable mouse usage",
+	"     [-listen N]             Enable multiplayer with max N players",
+	"     [-heapsize Bytes]       Heapsize (memory to allocate)",
+	NULL
+};
+
+static void PrintHelp (const char *name)
+{
+	int i = 0;
+
+	Sys_Printf ("Usage: %s [options]\n", name);
+	while (help_strings[i])
+	{
+		Sys_PrintTerm (help_strings[i]);
+		Sys_PrintTerm ("\n");
+		i++;
+	}
+	Sys_PrintTerm ("\n");
+}
+
+/*
+===============================================================================
+
+MAIN
+
+===============================================================================
+*/
+static quakeparms_t	parms;
+static char	cwd[MAX_OSPATH];
+#if defined(SDLQUAKE)
+static Uint8		appState;
+#endif
+
+int main (int argc, char **argv)
+{
+	int			i;
+	double		time, oldtime, newtime;
+
+	PrintVersion();
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if ( !(strcmp(argv[i], "-v")) || !(strcmp(argv[i], "-version" )) ||
+				!(strcmp(argv[i], "--version")) )
+			{
+				exit(0);
+			}
+			else if ( !(strcmp(argv[i], "-h")) || !(strcmp(argv[i], "-help" )) ||
+				  !(strcmp(argv[i], "-?")) || !(strcmp(argv[i], "--help")) )
+			{
+				PrintHelp(argv[0]);
+				exit (0);
+			}
+		}
+	}
+
+	memset (cwd, 0, sizeof(cwd));
+	if (Sys_GetBasedir(argv[0], cwd, sizeof(cwd)) != 0)
+		Sys_Error ("Couldn't determine current directory");
+
+	/* initialize the host params */
+	memset (&parms, 0, sizeof(parms));
+	parms.basedir = cwd;
+	parms.userdir = cwd;
+	parms.argc = argc;
+	parms.argv = argv;
+	host_parms = &parms;
+
+	LOG_Init (&parms);
+
+	Sys_Printf("basedir is: %s\n", parms.basedir);
+	Sys_Printf("userdir is: %s\n", parms.userdir);
+
+	COM_ValidateByteorder ();
+
+	isDedicated = (COM_CheckParm ("-dedicated") != 0);
+
+	Sys_CheckSDL ();
+
+	if (isDedicated)
+		parms.memsize = MIN_MEM_ALLOC;
+	else
+		parms.memsize = STD_MEM_ALLOC;
+
+	i = COM_CheckParm ("-heapsize");
+	if (i && i < com_argc-1)
+	{
+		parms.memsize = atoi (com_argv[i+1]) * 1024;
+
+		if ((parms.memsize > MAX_MEM_ALLOC) && !(COM_CheckParm ("-forcemem")))
+		{
+			Sys_Printf ("Requested memory (%d Mb) too large, using the default maximum.\n", parms.memsize/(1024*1024));
+			Sys_Printf ("If you are sure, use the -forcemem switch.\n");
+			parms.memsize = MAX_MEM_ALLOC;
+		}
+		else if ((parms.memsize < MIN_MEM_ALLOC) && !(COM_CheckParm ("-forcemem")))
+		{
+			Sys_Printf ("Requested memory (%d Mb) too little, using the default minimum.\n", parms.memsize/(1024*1024));
+			Sys_Printf ("If you are sure, use the -forcemem switch.\n");
+			parms.memsize = MIN_MEM_ALLOC;
+		}
+	}
+
+	parms.membase = malloc (parms.memsize);
+
+	if (!parms.membase)
+		Sys_Error ("Insufficient memory.\n");
+
+	Sys_Init ();
+
+	Host_Init();
+
+	oldtime = Sys_DoubleTime ();
+
+	/* main window message loop */
+	while (1)
+	{
+	    if (isDedicated)
+	    {
+		newtime = Sys_DoubleTime ();
+		time = newtime - oldtime;
+
+		while (time < sys_ticrate.value )
+		{
+			Sys_Sleep(1);
+			newtime = Sys_DoubleTime ();
+			time = newtime - oldtime;
+		}
+
+		Host_Frame (time);
+		oldtime = newtime;
+	    }
+	    else
+	    {
+#if defined(SDLQUAKE)
+		appState = SDL_GetAppState();
+		/* If we have no input focus at all, sleep a bit */
+		if ( !(appState & (SDL_APPMOUSEFOCUS | SDL_APPINPUTFOCUS)) || cl.paused)
+		{
+			Sys_Sleep(16);
+		}
+		/* If we're minimised, sleep a bit more */
+		if ( !(appState & SDL_APPACTIVE))
+		{
+			scr_skipupdate = 1;
+			Sys_Sleep(32);
+		}
+		else
+		{
+			scr_skipupdate = 0;
+		}
+#endif	/* SDLQUAKE */
+		newtime = Sys_DoubleTime ();
+		time = newtime - oldtime;
+
+		Host_Frame (time);
+
+		if (time < sys_throttle.value)
+			Sys_Sleep(1);
+
+		oldtime = newtime;
+	    }
+	}
+
+	return 0;
+}
+
