345a7e7c3b0f06b508f5d9441c6f11d70c34f13e
Ozkan Sezer <sezeroz@gmail.com>
2012-08-28 09:50:34 +0000

    h2patch: added new Makefile.darwin and a cross-build script for osx.
    git-svn-id: file:///svn/uhexen2/code/trunk@4904 eaf649e6-6c13-0410-b194-84221022858e

---
+++ h2patch/build_osx.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+rm -f	h2patch.ppc \
+	h2patch.x86 \
+	h2patch.x86_64 \
+	h2patch.bin
+make -f Makefile.darwin distclean
+
+OLDPATH=$PATH
+MAKE_CMD=make
+
+# ppc
+PATH=/opt/cross_osx-ppc/bin:$OLDPATH
+CC=powerpc-apple-darwin9-gcc
+AR=powerpc-apple-darwin9-ar
+RANLIB=powerpc-apple-darwin9-ranlib
+LIPO=powerpc-apple-darwin9-lipo
+export PATH CC AR RANLIB LIPO
+$MAKE_CMD -f Makefile.darwin MACH_TYPE=ppc $* || exit 1
+powerpc-apple-darwin9-strip -S h2patch || exit 1
+mv h2patch h2patch.ppc || exit 1
+$MAKE_CMD distclean
+
+# x86
+PATH=/opt/cross_osx-x86/bin:$OLDPATH
+CC=i686-apple-darwin9-gcc
+AR=i686-apple-darwin9-ar
+RANLIB=i686-apple-darwin9-ranlib
+LIPO=i686-apple-darwin9-lipo
+export PATH CC AR RANLIB LIPO
+$MAKE_CMD -f Makefile.darwin MACH_TYPE=x86 $* || exit 1
+i686-apple-darwin9-strip -S h2patch || exit 1
+mv h2patch h2patch.x86 || exit 1
+$MAKE_CMD distclean
+
+# x86_64
+PATH=/opt/cross_osx-x86_64/usr/bin:$OLDPATH
+CC=x86_64-apple-darwin9-gcc
+AR=x86_64-apple-darwin9-ar
+RANLIB=x86_64-apple-darwin9-ranlib
+LIPO=x86_64-apple-darwin9-lipo
+export PATH CC AR RANLIB LIPO
+$MAKE_CMD -f Makefile.darwin MACH_TYPE=x86_64 $* || exit 1
+x86_64-apple-darwin9-strip -S h2patch || exit 1
+mv h2patch h2patch.x86_64 || exit 1
+$MAKE_CMD distclean
+
+$LIPO -create -o h2patch.bin h2patch.ppc h2patch.x86 h2patch.x86_64 || exit 1
+
