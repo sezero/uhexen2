fb8b6c269b6e9cac2bfa4dec5b666c19b5e1c2e3
Ozkan Sezer <sezeroz@gmail.com>
2012-09-02 14:01:29 +0000

    - oslibs, codecs: minor libmad update - minor osx cross-build scripts update
    git-svn-id: file:///svn/uhexen2/code/trunk@4911 eaf649e6-6c13-0410-b194-84221022858e

--- engine/hexen2/server/build_cross_osx.sh
+++ engine/hexen2/server/build_cross_osx.sh
@@ -12,10 +12,11 @@ MAKE_CMD=make
 # ppc
 PATH=/opt/cross_osx-ppc/bin:$OLDPATH
 CC=powerpc-apple-darwin9-gcc
+AS=powerpc-apple-darwin9-as
 AR=powerpc-apple-darwin9-ar
 RANLIB=powerpc-apple-darwin9-ranlib
 LIPO=powerpc-apple-darwin9-lipo
-export PATH CC AR RANLIB LIPO
+export PATH CC AS AR RANLIB LIPO
 MAKE_FLAGS="MACH_TYPE=ppc TARGET_OS=unix HOST_OS=darwin"
 $MAKE_CMD $MAKE_FLAGS $* || exit 1
 powerpc-apple-darwin9-strip -S h2ded || exit 1
@@ -25,10 +26,11 @@ $MAKE_CMD distclean
 # x86
 PATH=/opt/cross_osx-x86/bin:$OLDPATH
 CC=i686-apple-darwin9-gcc
+AS=i686-apple-darwin9-as
 AR=i686-apple-darwin9-ar
 RANLIB=i686-apple-darwin9-ranlib
 LIPO=i686-apple-darwin9-lipo
-export PATH CC AR RANLIB LIPO
+export PATH CC AS AR RANLIB LIPO
 MAKE_FLAGS="MACH_TYPE=x86 TARGET_OS=unix HOST_OS=darwin"
 $MAKE_CMD $MAKE_FLAGS $* || exit 1
 i686-apple-darwin9-strip -S h2ded || exit 1
@@ -38,10 +40,11 @@ $MAKE_CMD distclean
 # x86_64
 PATH=/opt/cross_osx-x86_64/usr/bin:$OLDPATH
 CC=x86_64-apple-darwin9-gcc
+AS=x86_64-apple-darwin9-as
 AR=x86_64-apple-darwin9-ar
 RANLIB=x86_64-apple-darwin9-ranlib
 LIPO=x86_64-apple-darwin9-lipo
-export PATH CC AR RANLIB LIPO
+export PATH CC AS AR RANLIB LIPO
 MAKE_FLAGS="MACH_TYPE=x86_64 TARGET_OS=unix HOST_OS=darwin"
 $MAKE_CMD $MAKE_FLAGS $* || exit 1
 x86_64-apple-darwin9-strip -S h2ded || exit 1
