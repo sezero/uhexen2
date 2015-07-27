0b103bf439f1d3043d09ce0f72bc126ac07a6c55
Ozkan Sezer <sezeroz@gmail.com>
2015-07-27 05:40:07 +0000

    Info_RemoveKey: use memmove instead of strcpy for overlapping memory areas.
    git-svn-id: file:///svn/uhexen2/code/trunk@5424 eaf649e6-6c13-0410-b194-84221022858e

--- engine/hexenworld/shared/info_str.c
+++ engine/hexenworld/shared/info_str.c
@@ -112,7 +112,7 @@ void Info_RemoveKey (char *s, const char *key)
 
 		if (!strcmp (key, pkey) )
 		{
-			strcpy (start, s);	// remove this part
+			memmove(start, s, strlen(s) + 1);	// remove this part
 			return;
 		}
 
