71a1f0929982a96bdc667ad1252430b850123150
Ozkan Sezer <sezeroz@gmail.com>
2015-09-28 00:01:21 +0000

    dos_fxmesa.c: minor tidy-up for the resolutions array
    git-svn-id: file:///svn/uhexen2/code/trunk@5448 eaf649e6-6c13-0410-b194-84221022858e

--- engine/h2shared/dos_fxmesa.c
+++ engine/h2shared/dos_fxmesa.c
@@ -54,35 +54,40 @@ static fxMesaSwapBuffers_f fxMesaSwapBuffers_fp;
 
 static fxMesaContext fc = NULL;
 
-static int findres (int *width, int *height)
-{
-	static int fx_resolutions[][3] = {
-		{ 320,200,  GR_RESOLUTION_320x200 },
-		{ 320,240,  GR_RESOLUTION_320x240 },
-		{ 400,256,  GR_RESOLUTION_400x256 },
-		{ 400,300,  GR_RESOLUTION_400x300 },
-		{ 512,384,  GR_RESOLUTION_512x384 },
-		{ 640,200,  GR_RESOLUTION_640x200 },
-		{ 640,350,  GR_RESOLUTION_640x350 },
-		{ 640,400,  GR_RESOLUTION_640x400 },
-		{ 640,480,  GR_RESOLUTION_640x480 },
-		{ 800,600,  GR_RESOLUTION_800x600 },
-		{ 960,720,  GR_RESOLUTION_960x720 },
-		{ 856,480,  GR_RESOLUTION_856x480 },
-		{ 512,256,  GR_RESOLUTION_512x256 },
-		{ 1024,768, GR_RESOLUTION_1024x768 },
-		{ 1280,1024,GR_RESOLUTION_1280x1024 },
-		{ 1600,1200,GR_RESOLUTION_1600x1200 },
-		{ 0, 0, 0 }
-	};
+typedef struct {
+	int width, height;
+	GrScreenResolution_t fxmode;
+} fxmode_t;
 
+static fxmode_t fx_modes[] = {
+	{ 320,200,  GR_RESOLUTION_320x200 },
+	{ 320,240,  GR_RESOLUTION_320x240 },
+	{ 400,256,  GR_RESOLUTION_400x256 },
+	{ 400,300,  GR_RESOLUTION_400x300 },
+	{ 512,384,  GR_RESOLUTION_512x384 },
+	{ 640,200,  GR_RESOLUTION_640x200 },
+	{ 640,350,  GR_RESOLUTION_640x350 },
+	{ 640,400,  GR_RESOLUTION_640x400 },
+	{ 640,480,  GR_RESOLUTION_640x480 },
+	{ 800,600,  GR_RESOLUTION_800x600 },
+	{ 960,720,  GR_RESOLUTION_960x720 },
+	{ 856,480,  GR_RESOLUTION_856x480 },
+	{ 512,256,  GR_RESOLUTION_512x256 },
+	{ 1024,768, GR_RESOLUTION_1024x768 },
+	{ 1280,1024,GR_RESOLUTION_1280x1024 },
+	{ 1600,1200,GR_RESOLUTION_1600x1200 },
+};
+#define MAX_FXMODES (int)(sizeof(fx_modes) / sizeof(fx_modes[0]))
+
+static GrScreenResolution_t findres (int *width, int *height)
+{
 	int i;
 
-	for (i = 0; fx_resolutions[i][0]; i++) {
-		if (*width <= fx_resolutions[i][0] && *height <= fx_resolutions[i][1]) {
-			*width = fx_resolutions[i][0];
-			*height = fx_resolutions[i][1];
-			return fx_resolutions[i][2];
+	for (i = 0; i < MAX_FXMODES; i++) {
+		if (*width <= fx_modes[i].width && *height <= fx_modes[i].height) {
+			*width = fx_modes[i].width;
+			*height = fx_modes[i].height;
+			return fx_modes[i].fxmode;
 		}
 	}
 
