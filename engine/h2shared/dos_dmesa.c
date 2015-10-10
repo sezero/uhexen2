06c020240d3254465cf1a78fd765341585fde7c3
Ozkan Sezer <sezeroz@gmail.com>
2015-10-10 08:15:09 +0000

    dos_dmesa.c: create context in 565 format (no alpha) in 16 bit. use 24 bit depth buffer and also 8 bit stencil buffer in 32 bit.
    git-svn-id: file:///svn/uhexen2/code/trunk@5465 eaf649e6-6c13-0410-b194-84221022858e

--- engine/h2shared/dos_dmesa.c
+++ engine/h2shared/dos_dmesa.c
@@ -70,7 +70,10 @@ static DMesaBuffer db;
 
 static int DMESA_InitCtx (int *width, int *height, int *bpp)
 {
-	dv = DMesaCreateVisual_fp(*width, *height, *bpp, 0, true, true, 2, 16, 0, 0);
+	/* request either 16 or 32 bpp visual */
+	if (*bpp <= 16) /* 565 */
+		dv = DMesaCreateVisual_fp(*width, *height, 16, 0, true, true, 0, 16, 0, 0);
+	else	dv = DMesaCreateVisual_fp(*width, *height, 32, 0, true, true, 8, 24, 8, 0);
 	if (!dv) return -1;
 
 	dc = DMesaCreateContext_fp(dv, NULL);
