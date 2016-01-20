d492f4062fee940bd3d81d3caa4cb51a2120c9da
Ozkan Sezer <sezeroz@gmail.com>
2016-01-20 07:37:27 +0000

    added cross-build scripts for x86-AROS
    git-svn-id: file:///svn/uhexen2/code/trunk@5511 eaf649e6-6c13-0410-b194-84221022858e

---
+++ engine/hexen2/build_cross_aros.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# for x86-AROS cross-builds
+
+UHEXEN2_TOP=../..
+. $UHEXEN2_TOP/scripts/cross_defs.aros
+
+if test "$1" = "strip"; then
+	$STRIPPER -S hexen2 glhexen2 server/h2ded
+	exit 0
+fi
+
+HOST_OS=`uname|sed -e s/_.*//|tr '[:upper:]' '[:lower:]'`
+case "$HOST_OS" in
+freebsd|openbsd|netbsd)
+	MAKE_CMD=gmake ;;
+linux)	MAKE_CMD=make ;;
+*)	MAKE_CMD=make ;;
+esac
+
+if test "$1" = "h2ded"; then
+	shift
+	$MAKE_CMD -C server $* || exit 1
+	exit 0
+fi
+if test "$1" = "all"; then
+	shift
+
+	$MAKE_CMD clean
+	$MAKE_CMD h2 $* || exit 1
+
+	$MAKE_CMD clean
+	$MAKE_CMD glh2 $* || exit 1
+
+	$MAKE_CMD -C server clean
+	$MAKE_CMD -C server $* || exit 1
+
+	$MAKE_CMD clean
+	$MAKE_CMD -C server clean
+
+	exit 0
+fi
+
+exec $MAKE_CMD $*
+
---
+++ hw_utils/hwrcon/build_cross_aros.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# for x86-AROS cross-builds
+
+UHEXEN2_TOP=../..
+. $UHEXEN2_TOP/scripts/cross_defs.aros
+
+if test "$1" = "strip"; then
+	$STRIPPER -S hwrcon hwterm
+	exit 0
+fi
+
+HOST_OS=`uname|sed -e s/_.*//|tr '[:upper:]' '[:lower:]'`
+case "$HOST_OS" in
+freebsd|openbsd|netbsd)
+	MAKE_CMD=gmake ;;
+linux)	MAKE_CMD=make ;;
+*)	MAKE_CMD=make ;;
+esac
+
+exec $MAKE_CMD $*
+
