sezero/uhexen2

commit 4ce258fb8b6b3f34e9615404d7ca12c9ccebac1d

sys_amiga.c: fixes to Sys_FindFirst() and Sys_FindNext() - it's safe to leave the AnchorPath flags as 0 because according   to docs APF_DOWILD does nothing (wildcards are always matched.) - properly filter out directories.
git-svn-id: file:///svn/uhexen2/code/trunk@5885 eaf649e6-6c13-0410-b194-84221022858e

utils/common/util_io.c
@@ -492,13 +492,13 @@ const char *Q_FindFirstFile (const char *path, const char *pattern)
 	if (apath)
 		COM_Error ("Sys_FindFirst without FindClose");
 
-	apath = AllocVec (sizeof(struct AnchorPath) + PATH_SIZE, MEMF_CLEAR);
+	apath = (struct AnchorPath *) AllocVec (sizeof(struct AnchorPath) + PATH_SIZE, MEMF_CLEAR);
 	if (!apath)
 		return NULL;
 
 	apath->ap_Strlen = PATH_SIZE;
 	apath->ap_BreakBits = 0;
-	apath->ap_Flags = APB_DOWILD | !APB_DODIR;
+	apath->ap_Flags = 0;  /* APF_DOWILD */
 
 	newdir = Lock((const STRPTR) path, SHARED_LOCK);
 	if (newdir)
@@ -513,9 +513,12 @@ const char *Q_FindFirstFile (const char *path, const char *pattern)
 	pattern_str = pattern_helper (pattern);
 
 	if (MatchFirst((const STRPTR) pattern_str, apath) == 0)
+	{
+	    if (apath->ap_Info.fib_DirEntryType < 0)
 		return (const char *) (apath->ap_Info.fib_FileName);
+	}
 
-	return NULL;
+	return Sys_FindNextFile();
 }
 
 const char *Q_FindNextFile (void)
@@ -523,8 +526,11 @@ const char *Q_FindNextFile (void)
 	if (!apath)
 		return NULL;
 
-	if (MatchNext(apath) == 0)
+	while (MatchNext(apath) == 0)
+	{
+	    if (apath->ap_Info.fib_DirEntryType < 0)
 		return (const char *) (apath->ap_Info.fib_FileName);
+	}
 
 	return NULL;
 }

engine/hexen2/server/sys_amiga.c
@@ -293,13 +293,13 @@ const char *Sys_FindFirstFile (const char *path, const char *pattern)
 	if (apath)
 		Sys_Error ("Sys_FindFirst without FindClose");
 
-	apath = AllocVec (sizeof(struct AnchorPath) + PATH_SIZE, MEMF_CLEAR);
+	apath = (struct AnchorPath *) AllocVec (sizeof(struct AnchorPath) + PATH_SIZE, MEMF_CLEAR);
 	if (!apath)
 		return NULL;
 
 	apath->ap_Strlen = PATH_SIZE;
 	apath->ap_BreakBits = 0;
-	apath->ap_Flags = APB_DOWILD | !APB_DODIR;
+	apath->ap_Flags = 0;  /* APF_DOWILD */
 
 	newdir = Lock((const STRPTR) path, SHARED_LOCK);
 	if (newdir)
@@ -314,9 +314,12 @@ const char *Sys_FindFirstFile (const char *path, const char *pattern)
 	pattern_str = pattern_helper (pattern);
 
 	if (MatchFirst((const STRPTR) pattern_str, apath) == 0)
+	{
+	    if (apath->ap_Info.fib_DirEntryType < 0)
 		return (const char *) (apath->ap_Info.fib_FileName);
+	}
 
-	return NULL;
+	return Sys_FindNextFile();
 }
 
 const char *Sys_FindNextFile (void)
@@ -324,8 +327,11 @@ const char *Sys_FindNextFile (void)
 	if (!apath)
 		return NULL;
 
-	if (MatchNext(apath) == 0)
+	while (MatchNext(apath) == 0)
+	{
+	    if (apath->ap_Info.fib_DirEntryType < 0)
 		return (const char *) (apath->ap_Info.fib_FileName);
+	}
 
 	return NULL;
 }

engine/hexen2/sys_amiga.c
@@ -304,13 +304,13 @@ const char *Sys_FindFirstFile (const char *path, const char *pattern)
 	if (apath)
 		Sys_Error ("Sys_FindFirst without FindClose");
 
-	apath = AllocVec (sizeof(struct AnchorPath) + PATH_SIZE, MEMF_CLEAR);
+	apath = (struct AnchorPath *) AllocVec (sizeof(struct AnchorPath) + PATH_SIZE, MEMF_CLEAR);
 	if (!apath)
 		return NULL;
 
 	apath->ap_Strlen = PATH_SIZE;
 	apath->ap_BreakBits = 0;
-	apath->ap_Flags = APB_DOWILD | !APB_DODIR;
+	apath->ap_Flags = 0;  /* APF_DOWILD */
 
 	newdir = Lock((const STRPTR) path, SHARED_LOCK);
 	if (newdir)
@@ -325,9 +325,12 @@ const char *Sys_FindFirstFile (const char *path, const char *pattern)
 	pattern_str = pattern_helper (pattern);
 
 	if (MatchFirst((const STRPTR) pattern_str, apath) == 0)
+	{
+	    if (apath->ap_Info.fib_DirEntryType < 0)
 		return (const char *) (apath->ap_Info.fib_FileName);
+	}
 
-	return NULL;
+	return Sys_FindNextFile();
 }
 
 const char *Sys_FindNextFile (void)
@@ -335,8 +338,11 @@ const char *Sys_FindNextFile (void)
 	if (!apath)
 		return NULL;
 
-	if (MatchNext(apath) == 0)
+	while (MatchNext(apath) == 0)
+	{
+	    if (apath->ap_Info.fib_DirEntryType < 0)
 		return (const char *) (apath->ap_Info.fib_FileName);
+	}
 
 	return NULL;
 }

engine/hexenworld/client/sys_amiga.c
@@ -299,13 +299,13 @@ const char *Sys_FindFirstFile (const char *path, const char *pattern)
 	if (apath)
 		Sys_Error ("Sys_FindFirst without FindClose");
 
-	apath = AllocVec (sizeof(struct AnchorPath) + PATH_SIZE, MEMF_CLEAR);
+	apath = (struct AnchorPath *) AllocVec (sizeof(struct AnchorPath) + PATH_SIZE, MEMF_CLEAR);
 	if (!apath)
 		return NULL;
 
 	apath->ap_Strlen = PATH_SIZE;
 	apath->ap_BreakBits = 0;
-	apath->ap_Flags = APB_DOWILD | !APB_DODIR;
+	apath->ap_Flags = 0;  /* APF_DOWILD */
 
 	newdir = Lock((const STRPTR) path, SHARED_LOCK);
 	if (newdir)
@@ -320,9 +320,12 @@ const char *Sys_FindFirstFile (const char *path, const char *pattern)
 	pattern_str = pattern_helper (pattern);
 
 	if (MatchFirst((const STRPTR) pattern_str, apath) == 0)
+	{
+	    if (apath->ap_Info.fib_DirEntryType < 0)
 		return (const char *) (apath->ap_Info.fib_FileName);
+	}
 
-	return NULL;
+	return Sys_FindNextFile();
 }
 
 const char *Sys_FindNextFile (void)
@@ -330,8 +333,11 @@ const char *Sys_FindNextFile (void)
 	if (!apath)
 		return NULL;
 
-	if (MatchNext(apath) == 0)
+	while (MatchNext(apath) == 0)
+	{
+	    if (apath->ap_Info.fib_DirEntryType < 0)
 		return (const char *) (apath->ap_Info.fib_FileName);
+	}
 
 	return NULL;
 }

engine/hexenworld/server/sys_amiga.c
@@ -292,13 +292,13 @@ const char *Sys_FindFirstFile (const char *path, const char *pattern)
 	if (apath)
 		Sys_Error ("Sys_FindFirst without FindClose");
 
-	apath = AllocVec (sizeof(struct AnchorPath) + PATH_SIZE, MEMF_CLEAR);
+	apath = (struct AnchorPath *) AllocVec (sizeof(struct AnchorPath) + PATH_SIZE, MEMF_CLEAR);
 	if (!apath)
 		return NULL;
 
 	apath->ap_Strlen = PATH_SIZE;
 	apath->ap_BreakBits = 0;
-	apath->ap_Flags = APB_DOWILD | !APB_DODIR;
+	apath->ap_Flags = 0;  /* APF_DOWILD */
 
 	newdir = Lock((const STRPTR) path, SHARED_LOCK);
 	if (newdir)
@@ -313,9 +313,12 @@ const char *Sys_FindFirstFile (const char *path, const char *pattern)
 	pattern_str = pattern_helper (pattern);
 
 	if (MatchFirst((const STRPTR) pattern_str, apath) == 0)
+	{
+	    if (apath->ap_Info.fib_DirEntryType < 0)
 		return (const char *) (apath->ap_Info.fib_FileName);
+	}
 
-	return NULL;
+	return Sys_FindNextFile();
 }
 
 const char *Sys_FindNextFile (void)
@@ -323,8 +326,11 @@ const char *Sys_FindNextFile (void)
 	if (!apath)
 		return NULL;
 
-	if (MatchNext(apath) == 0)
+	while (MatchNext(apath) == 0)
+	{
+	    if (apath->ap_Info.fib_DirEntryType < 0)
 		return (const char *) (apath->ap_Info.fib_FileName);
+	}
 
 	return NULL;
 }

commit 33a9c8ce90c1a912ae6a1100d42b9ef2a2dabc1f

util_io.c: fix typo introduced in r5885
git-svn-id: file:///svn/uhexen2/code/trunk@5886 eaf649e6-6c13-0410-b194-84221022858e

utils/common/util_io.c
@@ -490,7 +490,7 @@ const char *Q_FindFirstFile (const char *path, const char *pattern)
 	BPTR newdir;
 
 	if (apath)
-		COM_Error ("Sys_FindFirst without FindClose");
+		COM_Error ("FindFirst without FindClose");
 
 	apath = (struct AnchorPath *) AllocVec (sizeof(struct AnchorPath) + PATH_SIZE, MEMF_CLEAR);
 	if (!apath)
@@ -518,7 +518,7 @@ const char *Q_FindFirstFile (const char *path, const char *pattern)
 		return (const char *) (apath->ap_Info.fib_FileName);
 	}
 
-	return Sys_FindNextFile();
+	return Q_FindNextFile();
 }
 
 const char *Q_FindNextFile (void)