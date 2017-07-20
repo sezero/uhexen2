2a31a627ef8c08afb1810799c2101527e6508746
Ozkan Sezer <sezeroz@gmail.com>
2017-07-20 14:01:02 +0000

    oslibs, amigaos3: make netinclude stuff build against clib2.
    git-svn-id: file:///svn/uhexen2/code/trunk@5906 eaf649e6-6c13-0410-b194-84221022858e

--- oslibs/amigaos/netinclude/sys/types.h
+++ oslibs/amigaos/netinclude/sys/types.h
@@ -1,3 +1,23 @@
+#ifdef __CLIB2__
+#include_next <sys/types.h>
+
+#ifndef clib2_missing_typedefs
+#define clib2_missing_typedefs
+/* missing typedefs : */
+typedef	unsigned char	u_char;
+typedef	unsigned short	u_short;
+typedef	unsigned int	u_int;
+typedef	unsigned long	u_long;
+typedef	unsigned short	ushort;
+typedef	unsigned int	uint;
+/* for fd_set & co */
+#define __NO_WAITSELECT
+#define __NO_NET_API
+#include <sys/select.h>
+#endif
+
+#else /* __CLIB2__ */
+
 /* workaround for ixemul headers >= 48.3:
  * AmiTCP SDK expect socklen_t as LONG,
  * however ixemul defines it as int... */
@@ -7,3 +27,5 @@
 #include_next <sys/types.h>
 #undef socklen_t
 #endif
+
+#endif /* __CLIB2__ */
