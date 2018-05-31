98bce63e5156e90be51490e1d0da6a07143e81c6
Ozkan Sezer <sezeroz@gmail.com>
2018-05-31 07:05:21 +0000

    ED_ParseEpair: don't read garbage into vectors if the string is too short
    from QuakeSpasm svn rev. 1527, patch by Eric Wasylishen.
    
    git-svn-id: file:///svn/uhexen2/code/trunk@6044 eaf649e6-6c13-0410-b194-84221022858e

--- engine/hexen2/pr_edict.c
+++ engine/hexen2/pr_edict.c
@@ -1023,6 +1023,7 @@ static qboolean	ED_ParseEpair (void *base, ddef_t *key, const char *s)
 	char	string[128];
 	ddef_t	*def;
 	char	*v, *w;
+	char	*end;
 	void	*d;
 	dfunction_t	*func;
 
@@ -1040,15 +1041,27 @@ static qboolean	ED_ParseEpair (void *base, ddef_t *key, const char *s)
 
 	case ev_vector:
 		q_strlcpy (string, s, sizeof(string));
+		end = (char *)string + strlen(string);
 		v = string;
 		w = string;
-		for (i = 0; i < 3; i++)
+
+		for (i = 0; i < 3 && (w <= end); i++) // ericw -- added (w <= end) check
 		{
+		// set v to the next space (or 0 byte), and change that char to a 0 byte
 			while (*v && *v != ' ')
 				v++;
 			*v = 0;
 			((float *)d)[i] = atof (w);
 			w = v = v+1;
+		}
+		// ericw -- fill remaining elements to 0 in case we hit the end of string
+		// before reading 3 floats.
+		if (i < 3)
+		{
+			Con_DPrintf ("Avoided reading garbage for \"%s\" \"%s\"\n",
+				     PR_GetString(key->s_name), s);
+			for (; i < 3; i++)
+				((float *)d)[i] = 0.0f;
 		}
 		break;
 
