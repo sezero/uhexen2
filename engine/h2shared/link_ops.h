b08f1cf9ca193638b2acbdbd39a6a7f6e79169e8
Ozkan Sezer <sezeroz@gmail.com>
2022-03-01 11:56:50 +0300

    Change STRUCT_FROM_LINK to avoid potential undefined behavior

--- engine/h2shared/link_ops.h
+++ engine/h2shared/link_ops.h
@@ -17,8 +17,8 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#ifndef __LINKOPS_H
-#define __LINKOPS_H
+#ifndef LINKOPS_H
+#define LINKOPS_H
 
 typedef struct link_s
 {
@@ -29,7 +29,7 @@ typedef struct link_s
 // (type *)STRUCT_FROM_LINK(link_t *link, type, member)
 // ent = STRUCT_FROM_LINK(link,entity_t,order)
 // FIXME: remove this mess!
-#define	STRUCT_FROM_LINK(l,t,m) ((t *)((byte *)l - (intptr_t)&(((t *)0)->m)))
+#define	STRUCT_FROM_LINK(l,t,m) ((t *)((byte *)l - offsetof(t,m)))
 
 
 void ClearLink (link_t *l);
@@ -38,5 +38,5 @@ void InsertLinkBefore (link_t *l, link_t *before);
 void InsertLinkAfter (link_t *l, link_t *after);
 
 
-#endif	/* __LINKOPS_H */
+#endif	/* LINKOPS_H */
 
