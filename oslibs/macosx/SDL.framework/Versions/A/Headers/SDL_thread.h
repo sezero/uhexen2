a40502c49a66961ef9b9ae2d38e6c6ba2430b4cb
BenderBlog "SuperBart" Rodriguez <Songze@live.cn>
2024-06-08 00:09:55 +0800

    update Xcode and SDL file

--- oslibs/macosx/SDL.framework/Versions/A/Headers/SDL_thread.h
+++ oslibs/macosx/SDL.framework/Versions/A/Headers/SDL_thread.h
@@ -1,118 +1,64 @@
 /*
-    SDL - Simple DirectMedia Layer
-    Copyright (C) 1997-2012 Sam Lantinga
+  Simple DirectMedia Layer
+  Copyright (C) 1997-2021 Sam Lantinga <slouken@libsdl.org>
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    version 2.1 of the License, or (at your option) any later version.
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
 
-    You should have received a copy of the GNU Lesser General Public
-    License along with this library; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-    Sam Lantinga
-    slouken@libsdl.org
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
 */
+
+/* These headers are from sdl12-compat, and are intended to give just enough
+functionality to let you build an SDL-1.2-based project without having the
+real SDL-1.2 available to you. */
 
 #ifndef _SDL_thread_h
 #define _SDL_thread_h
 
-/** @file SDL_thread.h
- *  Header for the SDL thread management routines 
- *
- *  @note These are independent of the other SDL routines.
- */
+#include <SDL/SDL_stdinc.h>
+#include <SDL/SDL_error.h>
+#include <SDL/SDL_mutex.h>
 
-#include "SDL_stdinc.h"
-#include "SDL_error.h"
-
-/* Thread synchronization primitives */
-#include "SDL_mutex.h"
-
-#include "begin_code.h"
-/* Set up for C function definitions, even when using C++ */
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-/** The SDL thread structure, defined in SDL_thread.c */
-struct SDL_Thread;
-typedef struct SDL_Thread SDL_Thread;
-
-/** Create a thread */
-#if ((defined(__WIN32__) && !defined(HAVE_LIBC)) || defined(__OS2__)) &&  !defined(__SYMBIAN32__)
-/**
- *  We compile SDL into a DLL on OS/2. This means, that it's the DLL which
- *  creates a new thread for the calling process with the SDL_CreateThread()
- *  API. There is a problem with this, that only the RTL of the SDL.DLL will
- *  be initialized for those threads, and not the RTL of the calling application!
- *  To solve this, we make a little hack here.
- *  We'll always use the caller's _beginthread() and _endthread() APIs to
- *  start a new thread. This way, if it's the SDL.DLL which uses this API,
- *  then the RTL of SDL.DLL will be used to create the new thread, and if it's
- *  the application, then the RTL of the application will be used.
- *  So, in short:
- *  Always use the _beginthread() and _endthread() of the calling runtime library!
- */
-#define SDL_PASSED_BEGINTHREAD_ENDTHREAD
-#ifndef _WIN32_WCE
-#include <process.h> /* This has _beginthread() and _endthread() defined! */
-#endif
+#ifdef __OS2__ /* for _beginthread() and _endthread(). */
+#include <process.h>
 #ifdef __EMX__
 #include <stdlib.h>
 #endif
+#endif
 
+#include <SDL/begin_code.h>
+
+typedef struct SDL_Thread SDL_Thread;
+
+/* No _beginthread()/_endthread() magic for windows -- only for os2:
+ * Official Windows versions of SDL-1.2 >= 1.2.10 were always built
+ * with HAVE_LIBC, i.e.: *without* SDL_PASSED_BEGINTHREAD_ENDTHREAD
+ * defined, in order to keep binary compatibility with SDL <= 1.2.9. */
 #ifdef __OS2__
-typedef int (*pfnSDL_CurrentBeginThread)(void (*func)(void *), void *, unsigned, void *arg); 
+typedef int (*pfnSDL_CurrentBeginThread)(void (*func)(void *), void *, unsigned, void *arg);
 typedef void (*pfnSDL_CurrentEndThread)(void);
-#else
-typedef uintptr_t (__cdecl *pfnSDL_CurrentBeginThread) (void *, unsigned,
-        unsigned (__stdcall *func)(void *), void *arg, 
-        unsigned, unsigned *threadID);
-typedef void (__cdecl *pfnSDL_CurrentEndThread)(unsigned code);
-#endif
-
 extern DECLSPEC SDL_Thread * SDLCALL SDL_CreateThread(int (SDLCALL *fn)(void *), void *data, pfnSDL_CurrentBeginThread pfnBeginThread, pfnSDL_CurrentEndThread pfnEndThread);
-
-#ifdef __OS2__
+#define SDL_PASSED_BEGINTHREAD_ENDTHREAD
 #define SDL_CreateThread(fn, data) SDL_CreateThread(fn, data, _beginthread, _endthread)
-#elif defined(_WIN32_WCE)
-#define SDL_CreateThread(fn, data) SDL_CreateThread(fn, data, NULL, NULL)
-#else
-#define SDL_CreateThread(fn, data) SDL_CreateThread(fn, data, _beginthreadex, _endthreadex)
-#endif
 #else
 extern DECLSPEC SDL_Thread * SDLCALL SDL_CreateThread(int (SDLCALL *fn)(void *), void *data);
 #endif
-
-/** Get the 32-bit thread identifier for the current thread */
 extern DECLSPEC Uint32 SDLCALL SDL_ThreadID(void);
-
-/** Get the 32-bit thread identifier for the specified thread,
- *  equivalent to SDL_ThreadID() if the specified thread is NULL.
- */
 extern DECLSPEC Uint32 SDLCALL SDL_GetThreadID(SDL_Thread *thread);
-
-/** Wait for a thread to finish.
- *  The return code for the thread function is placed in the area
- *  pointed to by 'status', if 'status' is not NULL.
- */
 extern DECLSPEC void SDLCALL SDL_WaitThread(SDL_Thread *thread, int *status);
-
-/** Forcefully kill a thread without worrying about its state */
 extern DECLSPEC void SDLCALL SDL_KillThread(SDL_Thread *thread);
 
+#include <SDL/close_code.h>
 
-/* Ends C function definitions when using C++ */
-#ifdef __cplusplus
-}
 #endif
-#include "close_code.h"
-
-#endif /* _SDL_thread_h */
